30431440138fcdc43513e5224829ca62e4fb7f65
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-30 08:57:54 +0900

    [Modify] .prettierrc.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 const App = () => {
   return (
     <div>
       <p className={`text-5xl`}>Hello</p>
+      <Button variant="contained">Primary</Button>
     </div>
   );
 };
