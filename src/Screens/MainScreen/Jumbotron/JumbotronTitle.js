00e6ba634c42ba41cd121ea23885d366fdd54760
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-30 12:02:29 +0900

    [Add] 점보트론 타이틀 추가.

--- src/Screens/MainScreen/Jumbotron/JumbotronTitle.js
+++ src/Screens/MainScreen/Jumbotron/JumbotronTitle.js
@@ -1,10 +1,30 @@
+import { Button } from "@mui/material";
+
 const JumbotronTitle = () => {
   return (
     <div>
-      <p className={`text-[32px] text-white`}>영어책 읽기가 처음인 우리 아이</p>
-      <p className={`text-[32px] text-white`}>
-        AI 스피킹으로 시작부터 남다르게!
-      </p>
+      <div className={`mb-[54px]`}>
+        <p className={`text-[50px] text-white`}>
+          영어책 읽기가 처음인 우리 아이
+        </p>
+        <p className={`text-[50px] text-white`}>
+          AI 스피킹으로 시작부터 남다르게!
+        </p>
+      </div>
+      <Button
+        variant={"outlined"}
+        sx={{
+          fontFamily: "Jua",
+          fontSize: 24,
+          color: "white",
+          letterSpacing: 1,
+          borderColor: "white",
+          width: 284,
+          height: 67
+        }}
+      >
+        무료 체험
+      </Button>
     </div>
   );
 };
