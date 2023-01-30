ffb3f4156d9baf306ada4b75fbdcbdfde81533cf
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-30 11:28:12 +0900

    [Add] 로고 링크 추가.

--- src/Screens/MainScreen/NavigationBar/Logo.js
+++ src/Screens/MainScreen/NavigationBar/Logo.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
-    <div>
+    <Link to={"/"}>
       <img
         src="/assets/images/Logo.png"
         alt="KidsBrown Logo"
         className={`w-[200px] h-[40px]`}
       />
-    </div>
+    </Link>
   );
 };
 
