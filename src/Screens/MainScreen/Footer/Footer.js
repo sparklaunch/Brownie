0bb1dbf8e8e056a341f52d5d753fab5a72990a29
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 08:37:33 +0900

    [Add] 푸터 상부 추가.

--- src/Screens/MainScreen/Footer/Footer.js
+++ src/Screens/MainScreen/Footer/Footer.js
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`border-t-2 border-[#DFDFDF] p-[40px] flex flex-col items-center`}
+    >
+      <div className={`flex flex-row items-center text-[14px] text-[#707070]`}>
+        <p className={`pr-3 border-r-2 border-[#DFDFDF]`}>(주)키즈브라운</p>
+        <p className={`pl-3`}>(04790) 서울특별시 성동구 성수일로 89, 605호</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
