f41e3ca8f4671fabf9983bdc7d2d4266a27e236d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-30 10:55:50 +0900

    [Install] 리액트 라우터 설치.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainScreen from "./screens/MainScreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <RecoilRoot>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<MainScreen />} />
+      </Routes>
+    </BrowserRouter>
   </RecoilRoot>
 );
