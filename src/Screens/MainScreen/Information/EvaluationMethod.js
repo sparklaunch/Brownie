d6a11b6786b6bca385d43303182905c1fa95ce17
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 08:11:58 +0900

    [Add] 평가 방식 추가.

--- src/Screens/MainScreen/Information/EvaluationMethod.js
+++ src/Screens/MainScreen/Information/EvaluationMethod.js
@@ -1,10 +1,14 @@
 const EvaluationMethod = () => {
   return (
-    <div>
-      <p>평가 방식</p>
-      <p>
-        <span>발음의 정확성과 유창성</span> 모두 측정 가능한 AI 기술을 이용하여,
-        정밀하게 측정하여 자동 평가합니다.
+    <div className={`flex flex-row items-center`}>
+      <p
+        className={`bg-[#1AB9C5] inline-block text-white text-[20px] p-2 px-5 rounded-md`}
+      >
+        평가 방식
+      </p>
+      <p className={`ml-5 text-[18px]`}>
+        <span className={`text-[#1AB9C5]`}>발음의 정확성과 유창성</span> 모두
+        측정 가능한 AI 기술을 이용하여, 정밀하게 측정하여 자동 평가합니다.
       </p>
     </div>
   );
