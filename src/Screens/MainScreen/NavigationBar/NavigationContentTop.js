87a542a125d35b6adc4d7e9241c2f6ba3cc0d1d2
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-30 11:36:19 +0900

    [Add] 연락처 추가.

--- src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
+++ src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 const NavigationContentTop = () => {
   return (
-    <div className={`justify-between`}>
+    <div className={`flex flex-row justify-between`}>
       <Link to={"/instructions"} className={`mr-[32px]`}>
         사용 설명서
       </Link>
-      <Link to={"/administrator"}>관리자 사이트</Link>
+      <Link to={"/administrator"} className={`mr-[32px]`}>
+        관리자 사이트
+      </Link>
+      <Link to={"tel:02-308-4088"} className={`flex flex-row items-center`}>
+        <LocalPhoneIcon fontSize={"small"} />
+        <p className={`ml-[12px]`}>02-308-4088 (내선 2번)</p>
+      </Link>
     </div>
   );
 };
