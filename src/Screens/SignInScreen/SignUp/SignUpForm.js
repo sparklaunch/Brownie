865a061a0e03f35a0a90251c8b85ba941a2375df
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 11:15:49 +0900

    [Add] 회원가입 헤더 추가.

--- src/Screens/SignInScreen/SignUp/SignUpForm.js
+++ src/Screens/SignInScreen/SignUp/SignUpForm.js
@@ -1,5 +1,11 @@
+import SignUpHeader from "./SignUpHeader";
+
 const SignUpForm = () => {
-  return <div></div>;
+  return (
+    <div className={`bg-[#F9F9F9] p-5`}>
+      <SignUpHeader />
+    </div>
+  );
 };
 
 export default SignUpForm;
---
+++ src/Screens/SignInScreen/SignUp/SignUpHeader.js
@@ -0,0 +1,10 @@
+const SignUpHeader = () => {
+  return (
+    <div className={`flex flex-row justify-between items-center mb-3`}>
+      <img src="assets/images/logo.png" alt="KidsBrown Logo" />
+      <p className={`text-[18px]`}>회원가입</p>
+    </div>
+  );
+};
+
+export default SignUpHeader;
