97502a44d41101e6d484b0b25a6153a90435fbe7
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 11:13:19 +0900

    [Add] 로그인 폼 헤더 여백 수정.

--- src/Screens/SignInScreen/SignIn/SignInHeader.js
+++ src/Screens/SignInScreen/SignIn/SignInHeader.js
@@ -1,6 +1,6 @@
 const SignInHeader = () => {
   return (
-    <div className={`flex flex-row justify-between items-center`}>
+    <div className={`flex flex-row justify-between items-center mb-3`}>
       <img src="assets/images/logo.png" alt="KidsBrown Logo" />
       <p className={`text-[18px]`}>로그인</p>
     </div>
