f7648495cb174762f16df5e4002187047f0f6174
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 09:14:31 +0900

    [Update] 맛보기 타이틀 마진 수정.

--- src/Screens/DemoScreen/TryTitle.js
+++ src/Screens/DemoScreen/TryTitle.js
@@ -1,6 +1,6 @@
 const TryTitle = () => {
   return (
-    <div className={`my-6`}>
+    <div className={`my-6 mt-12`}>
       <p className={`text-[32px] font-[Jua] text-center`}>체험하기</p>
     </div>
   );
