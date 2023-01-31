73ea229696e7701e2ab1c2268c9a1b361b3f561d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-31 09:12:49 +0900

    [Add] 내비게이션 바 그림자 추가.

--- src/Screens/MainScreen/NavigationBar/NavigationBar.js
+++ src/Screens/MainScreen/NavigationBar/NavigationBar.js
@@ -3,7 +3,7 @@ import NavigationContent from "./NavigationContent";
 
 const NavigationBar = () => {
   return (
-    <div className={`flex justify-center`}>
+    <div className={`flex justify-center shadow-md`}>
       <div
         className={`flex w-[1200px] h-[136px] flex-row justify-between items-center`}
       >
