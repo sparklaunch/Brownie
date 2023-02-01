140ab00b8d01450a28e2b39a3b636c3b9dd62ced
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-01 10:02:37 +0900

    [Fix] 모달 닫기 로직 수정.

--- src/Screens/MainScreen/Catalog/CatalogModal/CatalogModal.js
+++ src/Screens/MainScreen/Catalog/CatalogModal/CatalogModal.js
@@ -5,20 +5,22 @@ import { useRef } from "react";
 
 const CatalogModal = () => {
   const [modalOpen, setModalOpen] = useRecoilState(modalOpenAtom);
-  const contentRef = useRef(null);
+  const backdropRef = useRef(null);
   return (
     <div
       className={`fixed z-[1] top-0 left-0 bottom-0 right-0 backdrop-blur-md bg-white/30`}
       onClick={(event) => {
-        if (event.target !== contentRef.current) {
+        if (event.target === backdropRef.current) {
           setModalOpen(false);
         }
       }}
     >
-      <div className={`w-full h-full flex justify-center items-center`}>
+      <div
+        ref={backdropRef}
+        className={`w-full h-full flex justify-center items-center`}
+      >
         <div
           className={`w-[600px] p-3 bg-white rounded-lg shadow-xl border-t-8 border-[#1AB9C5]`}
-          ref={contentRef}
         >
           <CatalogModalContent />
         </div>
