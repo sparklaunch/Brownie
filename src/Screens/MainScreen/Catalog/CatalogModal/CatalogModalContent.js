19de02749e9021df170872866c800c88d69a155b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-01 09:30:32 +0900

    [Add] 교재 레이블 추가.

--- src/Screens/MainScreen/Catalog/CatalogModal/CatalogModalContent.js
+++ src/Screens/MainScreen/Catalog/CatalogModal/CatalogModalContent.js
@@ -1,10 +1,15 @@
 import selectedCatalogAtom from "../../../../Stores/Catalog/selectedCatalog";
 import { useRecoilState } from "recoil";
+import TextBookLabel from "./TextBookLabel";
 
 const CatalogModalContent = () => {
   const [selectedCatalog, setSelectedCatalog] =
     useRecoilState(selectedCatalogAtom);
-  return <div></div>;
+  return (
+    <div className={`p-5`}>
+      <TextBookLabel />
+    </div>
+  );
 };
 
 export default CatalogModalContent;
---
+++ src/Screens/MainScreen/Catalog/CatalogModal/TextBookLabel.js
@@ -0,0 +1,13 @@
+const TextBookLabel = () => {
+  return (
+    <div>
+      <p
+        className={`px-5 py-2 text-white bg-[#26C4B1] inline-block rounded-md text-[20px]`}
+      >
+        교재
+      </p>
+    </div>
+  );
+};
+
+export default TextBookLabel;
