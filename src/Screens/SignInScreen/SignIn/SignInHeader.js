2c01e8167323d0d696bf8ef8ffd30860575ba94b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 12:09:46 +0900

    [Update] 폼 로고 추가.

--- src/Screens/SignInScreen/SignIn/SignInHeader.js
+++ src/Screens/SignInScreen/SignIn/SignInHeader.js
@@ -1,7 +1,11 @@
 const SignInHeader = () => {
   return (
     <div className={`flex flex-row justify-between items-center mb-3`}>
-      <img src="assets/images/logo.png" alt="KidsBrown Logo" />
+      <img
+        src="assets/images/logos/main_logo.png"
+        alt="KidsBrown Logo"
+        className={`w-[180px]`}
+      />
       <p className={`text-[18px]`}>로그인</p>
     </div>
   );
--- src/Screens/SignInScreen/SignUp/SignUpHeader.js
+++ src/Screens/SignInScreen/SignUp/SignUpHeader.js
@@ -1,7 +1,11 @@
 const SignUpHeader = () => {
   return (
     <div className={`flex flex-row justify-between items-center mb-3`}>
-      <img src="assets/images/logo.png" alt="KidsBrown Logo" />
+      <img
+        src="assets/images/logos/main_logo.png"
+        alt="KidsBrown Logo"
+        className={`w-[180px]`}
+      />
       <p className={`text-[18px]`}>회원가입</p>
     </div>
   );
