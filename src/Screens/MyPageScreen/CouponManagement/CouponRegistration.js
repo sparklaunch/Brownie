96ca6601e9ec6037de6d2b4cfefe2b545c6dceb8
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 10:23:36 +0900

    [Add] 쿠폰 등록 폼 추가.

--- src/Screens/MyPageScreen/CouponManagement/CouponRegistration.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponRegistration.js
@@ -1,9 +1,36 @@
+import { Button, TextField } from "@mui/material";
+import { useRecoilState } from "recoil";
+import couponAtom from "../../../Stores/Auth/coupon";
+
 const CouponRegistration = () => {
+  const [coupon, setCoupon] = useRecoilState(couponAtom);
   return (
     <div>
       <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
         쿠폰 등록
       </p>
+      <div className={`flex flex-row`}>
+        <TextField
+          value={coupon}
+          onChange={(event) => setCoupon(event.target.value)}
+          variant={"outlined"}
+          size={"small"}
+          label={"예) ######"}
+          sx={{
+            width: "300px"
+          }}
+        />
+        <Button
+          variant={"contained"}
+          sx={{
+            marginLeft: "10px",
+            paddingX: "30px",
+            backgroundColor: "#222222"
+          }}
+        >
+          등록
+        </Button>
+      </div>
     </div>
   );
 };
