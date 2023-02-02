b94692e1f6250f4f399bf9a44cc02834770eac58
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 09:33:39 +0900

    [Add] 비밀번호 아톰 연결.

--- src/Screens/MyPageScreen/AccountInformation/PasswordField.js
+++ src/Screens/MyPageScreen/AccountInformation/PasswordField.js
@@ -1,11 +1,16 @@
 import { TextField } from "@mui/material";
+import { useRecoilState } from "recoil";
+import passwordAtom from "../../../Stores/Auth/password";
 
 const PasswordField = () => {
+  const [password, setPassword] = useRecoilState(passwordAtom);
   return (
     <div className={`flex flex-row justify-center`}>
       <TextField
         variant={"outlined"}
         label={"비밀번호"}
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
         type={"password"}
         size={"small"}
         sx={{
