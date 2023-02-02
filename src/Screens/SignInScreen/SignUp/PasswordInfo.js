15cfab5490e762b320dc0957bf0ba093409a18e5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 14:43:37 +0900

    [Update] 인포 아이콘 벡터로 수정.

--- src/Screens/SignInScreen/SignUp/PasswordInfo.js
+++ src/Screens/SignInScreen/SignUp/PasswordInfo.js
@@ -2,7 +2,7 @@ const PasswordInfo = () => {
   return (
     <div className={`mb-3`}>
       <div className={`flex flex-row items-center`}>
-        <img src={"assets/images/icons/info_icon.png"} alt={"Info Icon"} />
+        <img src={"assets/images/icons/info_icon.svg"} alt={"Info Icon"} />
         <p className={`text-[13px] text-[#ACACAC] ml-2`}>
           알파벳, 숫자 조합으로 최소 6자리 이상 입력하세요.
         </p>
