0216352f4ecc600ccccb669b51ecbb3628a97142
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 10:41:15 +0900

    [Add] 푸터 추가.

--- src/Screens/MyPageScreen/CouponManagement/CouponCaution.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponCaution.js
@@ -3,7 +3,7 @@ import CouponCautionDetail from "./CouponCautionDetail";
 
 const CouponCaution = () => {
   return (
-    <div className={`flex flex-row justify-center`}>
+    <div className={`flex flex-row justify-center my-12`}>
       <div className={`w-[800px] border-2 p-8 rounded-md bg-[#F9F9F9]`}>
         <CouponCautionTitle />
         <CouponCautionDetail />
--- src/Screens/MyPageScreen/CouponManagement/CouponManagement.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponManagement.js
@@ -1,15 +1,19 @@
 import CouponRegistration from "./CouponRegistration";
 import CouponHistory from "./CouponHistory";
 import CouponCaution from "./CouponCaution";
+import Footer from "../../MainScreen/Footer/Footer";
 
 const CouponManagement = () => {
   return (
-    <div className={`flex flex-row justify-center`}>
-      <div className={`w-[1200px]`}>
-        <CouponRegistration />
-        <CouponHistory />
-        <CouponCaution />
+    <div>
+      <div className={`flex flex-row justify-center`}>
+        <div className={`w-[1200px]`}>
+          <CouponRegistration />
+          <CouponHistory />
+          <CouponCaution />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
