90b9b51e20d22dbd6ac45fe4c9297327063a9612
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 09:01:00 +0900

    [Add] 내 정보 콘텐츠 추가.

--- src/Screens/MyPageScreen/Status/MyInformation.js
+++ src/Screens/MyPageScreen/Status/MyInformation.js
@@ -1,7 +1,20 @@
 const MyInformation = () => {
   return (
     <div>
-      <p className={`border-l-4 border-[#00AEA8] pl-3`}>내 정보</p>
+      <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
+        내 정보
+      </p>
+      <div
+        className={`p-8 border-[1px] border-[#DFDFDF] rounded-lg text-[18px] grid gap-3`}
+      >
+        <p>이름 : 홍길동</p>
+        <p>구매상품 : 패키지 A 상품</p>
+        <p>
+          이용쿠폰 :{" "}
+          <span className={`text-[#F34D7E]`}>A001 23 E01 06 0001</span>
+        </p>
+        <p>이용기간 : 2023.05.06 ~ 2023.12.31</p>
+      </div>
     </div>
   );
 };
