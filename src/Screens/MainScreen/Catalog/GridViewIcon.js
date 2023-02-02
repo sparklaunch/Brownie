4c045daa5f866082ea7ea67b9917801dcda79b55
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 13:12:07 +0900

    [Update] 카탈로그 필터 아이콘 변경.

--- src/Screens/MainScreen/Catalog/GridViewIcon.js
+++ src/Screens/MainScreen/Catalog/GridViewIcon.js
@@ -1,15 +1,11 @@
-import WindowIcon from "@mui/icons-material/Window";
-
 const GridViewIcon = () => {
   return (
     <div
       className={`w-[40px] h-[40px] flex justify-center items-center border-2 border-[#DFDFDF] border-solid rounded-md cursor-pointer mr-[5px]`}
     >
-      <WindowIcon
-        fontSize={"medium"}
-        sx={{
-          fill: "#707070"
-        }}
+      <img
+        src="assets/images/icons/gallery_view_icon.png"
+        alt="Gallery View Icon"
       />
     </div>
   );
--- src/Screens/MainScreen/Catalog/ListViewIcon.js
+++ src/Screens/MainScreen/Catalog/ListViewIcon.js
@@ -1,15 +1,11 @@
-import ViewListIcon from "@mui/icons-material/ViewList";
-
 const ListViewIcon = () => {
   return (
     <div
       className={`w-[40px] h-[40px] flex justify-center items-center border-2 border-[#DFDFDF] border-solid rounded-md cursor-pointer`}
     >
-      <ViewListIcon
-        fontSize={"medium"}
-        sx={{
-          fill: "#707070"
-        }}
+      <img
+        src={"assets/images/icons/list_view_icon.png"}
+        alt={"List View Icon"}
       />
     </div>
   );
