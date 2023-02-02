b1b4a538d40a2366886b93513caf87a46a3467a4
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 16:24:38 +0900

    [Add] 백그라운드 추가.

---
+++ src/Screens/ClassroomScreen/Background.js
@@ -0,0 +1,5 @@
+const Background = () => {
+  return <div className={`bg-[#E8F6F8] w-[100vw] h-[100vh]`} />;
+};
+
+export default Background;
--- src/Screens/TrialScreen/TrialScreen.js
+++ src/Screens/TrialScreen/TrialScreen.js
@@ -1,9 +1,14 @@
 import { useParams } from "react-router-dom";
+import Background from "../ClassroomScreen/Background";
 
 const TrialScreen = () => {
   const { level } = useParams();
   console.log(level);
-  return <div></div>;
+  return (
+    <div>
+      <Background />
+    </div>
+  );
 };
 
 export default TrialScreen;
