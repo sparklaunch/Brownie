d1da612977764b6fcbea5db7245e4a9aed71b5a9
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:30:51 +0900

    [Update] 갤러리 뷰 아이콘 벡터로 수정.

--- src/Screens/MainScreen/Catalog/GridViewIcon.js
+++ src/Screens/MainScreen/Catalog/GridViewIcon.js
@@ -4,7 +4,7 @@ const GridViewIcon = () => {
       className={`w-[40px] h-[40px] flex justify-center items-center border-2 border-[#DFDFDF] border-solid rounded-md cursor-pointer mr-[5px]`}
     >
       <img
-        src="assets/images/icons/gallery_view_icon.png"
+        src="assets/images/icons/gallery_view_icon.svg"
         alt="Gallery View Icon"
       />
     </div>
