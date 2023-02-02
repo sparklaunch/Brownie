93422c20898e545a5b63d38fb3cc6af31e2000e2
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:31:52 +0900

    [Update] 리스트 뷰 아이콘 벡터로 수정.

--- src/Screens/MainScreen/Catalog/ListViewIcon.js
+++ src/Screens/MainScreen/Catalog/ListViewIcon.js
@@ -4,7 +4,7 @@ const ListViewIcon = () => {
       className={`w-[40px] h-[40px] flex justify-center items-center border-2 border-[#DFDFDF] border-solid rounded-md cursor-pointer`}
     >
       <img
-        src={"assets/images/icons/list_view_icon.png"}
+        src={"assets/images/icons/list_view_icon.svg"}
         alt={"List View Icon"}
       />
     </div>
