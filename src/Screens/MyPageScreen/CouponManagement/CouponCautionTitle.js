7b984cd7cf49833a405aab304f1a440f43212779
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:49:21 +0900

    [Update] 인포 액센트 아이콘 벡터로 수정.

--- src/Screens/MyPageScreen/CouponManagement/CouponCautionTitle.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponCautionTitle.js
@@ -1,7 +1,7 @@
 const CouponCautionTitle = () => {
   return (
     <div className={`flex flex-row items-center justify-center mb-4`}>
-      <img src={"assets/images/icons/info_accent_icon.png"} alt={"Info Icon"} />
+      <img src={"assets/images/icons/info_accent_icon.svg"} alt={"Info Icon"} />
       <p className={`font-[Jua] text-[#1AB9C5] text-[22px] ml-2`}>
         꼭 확인해주세요!
       </p>
