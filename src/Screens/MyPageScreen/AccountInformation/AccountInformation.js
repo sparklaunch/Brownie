a810b7227b6d1e65ec7b9d700b1b7e87ec2e12ce
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 09:42:25 +0900

    [Add] 푸터 추가.

--- src/Screens/MyPageScreen/AccountInformation/AccountInformation.js
+++ src/Screens/MyPageScreen/AccountInformation/AccountInformation.js
@@ -2,16 +2,20 @@ import AccountIcon from "./AccountIcon";
 import AccountTitle from "./AccountTitle";
 import PasswordField from "./PasswordField";
 import AccountButtons from "./AccountButtons";
+import Footer from "../../MainScreen/Footer/Footer";
 
 const AccountInformation = () => {
   return (
-    <div className={`flex flex-row justify-center bg-[#F9F9F9] p-12`}>
-      <div className={`w-[1200px]`}>
-        <AccountIcon />
-        <AccountTitle />
-        <PasswordField />
-        <AccountButtons />
+    <div>
+      <div className={`flex flex-row justify-center bg-[#F9F9F9] p-[100px]`}>
+        <div className={`w-[1200px]`}>
+          <AccountIcon />
+          <AccountTitle />
+          <PasswordField />
+          <AccountButtons />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
