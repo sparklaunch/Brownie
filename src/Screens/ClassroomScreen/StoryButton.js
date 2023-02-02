1099dc74924d2f94ec39b07656ed31ec501bae02
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 16:42:43 +0900

    [Add] 활성화 모드에 따른 버튼 디자인 적용.

--- src/Screens/ClassroomScreen/StoryButton.js
+++ src/Screens/ClassroomScreen/StoryButton.js
@@ -5,7 +5,9 @@ const StoryButton = () => {
   const [mode, setMode] = useRecoilState(modeAtom);
   return (
     <div
-      className={`w-[100px] h-[100px] bg-white flex flex-col items-center justify-center rounded-lg shadow-md border-[3px] border-[transparent] cursor-pointer hover:border-[3px] hover:border-[#47C7D2] transition duration-200 ease-in-out`}
+      className={`w-[100px] h-[100px] bg-white flex flex-col items-center justify-center rounded-lg shadow-md border-[3px] border-[transparent] cursor-pointer ${
+        mode === "story" ? "border-[3px] border-[#47C7D2]" : ""
+      } transition duration-200 ease-in-out}`}
       onClick={() => setMode("story")}
     >
       <img src={"/assets/images/icons/story_icon.svg"} alt={"Story Icon"} />
--- src/Screens/ClassroomScreen/WordButton.js
+++ src/Screens/ClassroomScreen/WordButton.js
@@ -5,7 +5,9 @@ const WordButton = () => {
   const [mode, setMode] = useRecoilState(modeAtom);
   return (
     <div
-      className={`w-[100px] h-[100px] bg-white flex flex-col items-center justify-center rounded-lg shadow-md border-[3px] border-[transparent] cursor-pointer hover:border-[3px] hover:border-[#47C7D2] transition duration-200 ease-in-out`}
+      className={`w-[100px] h-[100px] bg-white flex flex-col items-center justify-center rounded-lg shadow-md border-[3px] border-[transparent] cursor-pointer ${
+        mode === "word" ? "border-[3px] border-[#47C7D2]" : ""
+      } transition duration-200 ease-in-out}`}
       onClick={() => setMode("word")}
     >
       <img src={"/assets/images/icons/word_icon.svg"} alt={"Word Icon"} />
