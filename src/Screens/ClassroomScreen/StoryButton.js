3c1361fa4f79557a6e500a35739a0c30662ced03
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 16:34:27 +0900

    [Add] 스토리 모드 버튼 추가.

--- src/Screens/ClassroomScreen/StoryButton.js
+++ src/Screens/ClassroomScreen/StoryButton.js
@@ -1,5 +1,12 @@
 const StoryButton = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`w-[100px] h-[100px] bg-white flex flex-col items-center justify-center rounded-lg shadow-md border-[3px] border-[transparent] cursor-pointer hover:border-[3px] hover:border-[#47C7D2] transition duration-200 ease-in-out`}
+    >
+      <img src={"/assets/images/icons/story_icon.svg"} alt={"Story Icon"} />
+      <p className={`font-[Jua] text-[20px]`}>Story</p>
+    </div>
+  );
 };
 
 export default StoryButton;
