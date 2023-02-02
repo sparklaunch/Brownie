1a2b2db1b1488a5fd2564d1fe97654124fe9ecad
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:46:26 +0900

    [Update] 피라미드 아이콘 벡터로 수정.

--- src/Screens/MyPageScreen/Status/StatusLabels.js
+++ src/Screens/MyPageScreen/Status/StatusLabels.js
@@ -2,12 +2,16 @@ const StatusLabels = () => {
   return (
     <div className={`flex flex-row justify-end`}>
       <div className={`flex flex-row items-center`}>
-        <img src={"assets/images/icons/bullet_icon.svg"} alt={"Bullet Icon"} />
+        <img
+          src={"assets/images/icons/bullet_icon.svg"}
+          alt={"Bullet Icon"}
+          className={`w-[14px] h-[14px]`}
+        />
         <p className={`text-[16px] ml-2`}>학습 완료</p>
       </div>
       <div className={`flex flex-row items-center ml-6`}>
         <img
-          src={"assets/images/icons/pyramid_icon.png"}
+          src={"assets/images/icons/pyramid_icon.svg"}
           alt={"Pyramid Icon"}
         />
         <p className={`text-[16px] ml-2`}>학습 진행중</p>
