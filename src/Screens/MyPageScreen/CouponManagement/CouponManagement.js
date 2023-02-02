dbf8f8948f10cabaea7b4b16165c554fc326b47d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 10:18:30 +0900

    [Add] 쿠폰 등록 레이블 추가.

--- src/Screens/MyPageScreen/CouponManagement/CouponManagement.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponManagement.js
@@ -1,5 +1,13 @@
+import CouponRegistration from "./CouponRegistration";
+
 const CouponManagement = () => {
-  return <div></div>;
+  return (
+    <div className={`flex flex-row justify-center`}>
+      <div className={`w-[1200px]`}>
+        <CouponRegistration />
+      </div>
+    </div>
+  );
 };
 
 export default CouponManagement;
---
+++ src/Screens/MyPageScreen/CouponManagement/CouponRegistration.js
@@ -0,0 +1,11 @@
+const CouponRegistration = () => {
+  return (
+    <div>
+      <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
+        쿠폰 등록
+      </p>
+    </div>
+  );
+};
+
+export default CouponRegistration;
