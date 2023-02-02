682d4dd7d266ed3b9dd8358e6867b353193ba791
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 09:11:35 +0900

    [Add] 학습 진행중 레이블 추가.

--- src/Screens/MyPageScreen/Status/StatusLabels.js
+++ src/Screens/MyPageScreen/Status/StatusLabels.js
@@ -1,8 +1,9 @@
 import CircleIcon from "@mui/icons-material/Circle";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const StatusLabels = () => {
   return (
-    <div>
+    <div className={`flex flex-row justify-end`}>
       <div className={`flex flex-row items-center`}>
         <CircleIcon
           fontSize={"small"}
@@ -11,6 +12,15 @@ const StatusLabels = () => {
           }}
         />
         <p className={`text-[16px] ml-2`}>학습 완료</p>
+      </div>
+      <div className={`flex flex-row items-center ml-3`}>
+        <WarningIcon
+          fontSize={"small"}
+          sx={{
+            fill: "#FF8200"
+          }}
+        />
+        <p className={`text-[16px] ml-2`}>학습 진행중</p>
       </div>
     </div>
   );
