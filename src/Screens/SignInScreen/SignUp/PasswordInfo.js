da651f496d7d1f0b8709884cd5c1c0999f5bbef2
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 13:20:37 +0900

    [Update] 정보 아이콘으로 변경.

--- src/Screens/SignInScreen/SignUp/PasswordInfo.js
+++ src/Screens/SignInScreen/SignUp/PasswordInfo.js
@@ -1,15 +1,9 @@
-import InfoIcon from "@mui/icons-material/Info";
-
 const PasswordInfo = () => {
   return (
     <div className={`mb-3`}>
       <div className={`flex flex-row items-center`}>
-        <InfoIcon
-          sx={{
-            fill: "#ACACAC"
-          }}
-        />
-        <p className={`text-[13px] text-[#ACACAC] ml-1`}>
+        <img src={"assets/images/icons/info_icon.png"} alt={"Info Icon"} />
+        <p className={`text-[13px] text-[#ACACAC] ml-2`}>
           알파벳, 숫자 조합으로 최소 6자리 이상 입력하세요.
         </p>
       </div>
