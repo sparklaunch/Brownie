0d86338f3a13e20854c7fa7d275311ff42b96e1f
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-03 08:58:01 +0900

    [Add] 페이지 터닝 기능 추가.

--- src/Screens/ClassroomScreen/LeftPageTurner.js
+++ src/Screens/ClassroomScreen/LeftPageTurner.js
@@ -1,7 +1,15 @@
+import { useRecoilState } from "recoil";
+import currentPageAtom from "../../Stores/Classroom/currentPage";
+
 const LeftPageTurner = () => {
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  const onClickLeftPageTurner = () => {
+    setCurrentPage(Math.max(currentPage - 2, 1));
+  };
   return (
     <div
       className={`absolute z-10 top-[50%] left-[10vw] translate-y-[-50%] cursor-pointer`}
+      onClick={onClickLeftPageTurner}
     >
       <img
         src={"/assets/images/icons/left_angle_icon.svg"}
--- src/Screens/ClassroomScreen/RightPageTurner.js
+++ src/Screens/ClassroomScreen/RightPageTurner.js
@@ -1,7 +1,15 @@
+import { useRecoilState } from "recoil";
+import currentPageAtom from "../../Stores/Classroom/currentPage";
+
 const RightPageTurner = () => {
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  const onClickRightPageTurner = () => {
+    setCurrentPage(Math.min(currentPage + 2, 19));
+  };
   return (
     <div
       className={`absolute z-10 top-[50%] right-[10vw] translate-y-[-50%] cursor-pointer`}
+      onClick={onClickRightPageTurner}
     >
       <img
         src={"/assets/images/icons/right_angle_icon.svg"}
