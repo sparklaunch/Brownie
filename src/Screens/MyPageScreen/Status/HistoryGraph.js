e318c08bdb4ff75c4f651043b1233a45261551bd
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 09:03:12 +0900

    [Add] 월별 독서량 추가.

---
+++ src/Screens/MyPageScreen/Status/HistoryGraph.js
@@ -0,0 +1,11 @@
+const HistoryGraph = () => {
+  return (
+    <div>
+      <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
+        최근 월별 독서량
+      </p>
+    </div>
+  );
+};
+
+export default HistoryGraph;
--- src/Screens/MyPageScreen/Status/Status.js
+++ src/Screens/MyPageScreen/Status/Status.js
@@ -1,11 +1,13 @@
 import MyInformation from "./MyInformation";
+import HistoryGraph from "./HistoryGraph";
 
 const Status = () => {
   return (
     <div className={`flex flex-row justify-center`}>
       <div className={`w-[1200px]`}>
-        <div>
+        <div className={`grid grid-cols-2 gap-5`}>
           <MyInformation />
+          <HistoryGraph />
         </div>
       </div>
     </div>
