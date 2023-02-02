73366323a3ff57cc74c6325696f2ac091fd26ea6
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 14:24:35 +0900

    [Update] 회원가입 폼에서 불릿 아이콘으로 변경.

--- src/Screens/SignInScreen/SignUp/StudentLabel.js
+++ src/Screens/SignInScreen/SignUp/StudentLabel.js
@@ -1,14 +1,10 @@
-import CircleIcon from "@mui/icons-material/Circle";
-
 const StudentLabel = () => {
   return (
     <div className={`flex flex-row items-center mb-1`}>
-      <CircleIcon
-        sx={{
-          width: "10px",
-          height: "10px",
-          fill: "#1AB9C5"
-        }}
+      <img
+        src={"assets/images/icons/bullet_icon.png"}
+        alt={"Bullet Icon"}
+        className={`w-[8px] h-[8px]`}
       />
       <p className={`ml-3 text-[18px]`}>학생(자녀) 정보</p>
     </div>
