0b0a2d2ab8b3afd5bf85286e7ebcefcab0873ab3
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:47:47 +0900

    [Update] 유저 아이콘 벡터로 수정.

--- src/Screens/MyPageScreen/AccountInformation/AccountIcon.js
+++ src/Screens/MyPageScreen/AccountInformation/AccountIcon.js
@@ -2,7 +2,7 @@ const AccountIcon = () => {
   return (
     <div className={`mb-6 flex flex-row justify-center`}>
       <img
-        src={"assets/images/icons/user_lock_icon.png"}
+        src={"assets/images/icons/user_lock_icon.svg"}
         alt={"User with Lock Icon"}
       />
     </div>
