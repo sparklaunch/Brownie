0bfcf6223b1cdf515856038d9dcaaf31f8cbcd60
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-02 15:28:50 +0900

    [Update] 수화기 아이콘 벡터로 수정.

--- src/Screens/MainScreen/NavigationBar/Contact.js
+++ src/Screens/MainScreen/NavigationBar/Contact.js
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div>
       <a href={`tel:02-308-4088`} className={`flex flex-row items-center`}>
-        <img src={"assets/images/icons/phone_icon.png"} alt={"Phone Icon"} />
+        <img src={"assets/images/icons/phone_icon.svg"} alt={"Phone Icon"} />
         <p className={`ml-[12px]`}>02-308-4088 (내선 2번)</p>
       </a>
     </div>
