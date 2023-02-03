de11124f9ee9c6fb9a4322df4c5d1d9a2f40c79c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-03 09:59:07 +0900

    [Add] 교재 이미지 레이지 로딩.

--- src/Screens/ClassroomScreen/LeftPage.js
+++ src/Screens/ClassroomScreen/LeftPage.js
@@ -1,7 +1,7 @@
 const LeftPage = ({ fileName }) => {
   return (
     <div className={`relative`}>
-      <img src={fileName} alt={"Left Page"} />
+      <img src={fileName} alt={"Left Page"} loading={"lazy"} />
       <div
         className={`absolute right-0 top-0 h-full w-10 bg-gradient-to-r from-transparent to-black opacity-50`}
       />
--- src/Screens/ClassroomScreen/RightPage.js
+++ src/Screens/ClassroomScreen/RightPage.js
@@ -1,7 +1,7 @@
 const RightPage = ({ fileName }) => {
   return (
     <div className={`relative`}>
-      <img src={fileName} alt={"Right Page"} />
+      <img src={fileName} alt={"Right Page"} loading={"lazy"} />
       <div
         className={`absolute left-0 top-0 h-full w-10 bg-gradient-to-r from-black opacity-50`}
       />
