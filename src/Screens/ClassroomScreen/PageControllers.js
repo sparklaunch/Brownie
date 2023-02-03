0142746d1d6e597fded95705094a6c105c0dd9aa
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-03 09:33:53 +0900

    [Add] 페이지 이동 버튼 추가.

--- src/Screens/ClassroomScreen/PageControllers.js
+++ src/Screens/ClassroomScreen/PageControllers.js
@@ -6,11 +6,17 @@ const PageControllers = () => {
   const onClickFirstPage = () => {
     setCurrentPage(1);
   };
+  const onLeftPageButtonClick = () => {
+    setCurrentPage(Math.max(currentPage - 2, 1));
+  };
+  const onRightPageButtonClick = () => {
+    setCurrentPage(Math.min(currentPage + 2, 19));
+  };
   return (
     <div className={`absolute top-[4vw] right-[15vw]`}>
       <div className={`flex flex-row`}>
         <p
-          className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl cursor-pointer shadow-md`}
+          className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl cursor-pointer shadow-md mr-3`}
           onClick={onClickFirstPage}
         >
           처음으로
@@ -19,6 +25,8 @@ const PageControllers = () => {
           <img
             src={"/assets/images/icons/left_page_button.svg"}
             alt={`Left Page Button`}
+            className={`cursor-pointer mr-2`}
+            onClick={onLeftPageButtonClick}
           />
           <p
             className={`bg-white border-[1px] border-[#1AB9C5] rounded-3xl w-[120px] flex flex-row text-[28px] items-center justify-center`}
@@ -29,6 +37,8 @@ const PageControllers = () => {
           <img
             src={"/assets/images/icons/right_page_button.svg"}
             alt={`Right Page Button`}
+            className={`cursor-pointer ml-2`}
+            onClick={onRightPageButtonClick}
           />
         </div>
       </div>
