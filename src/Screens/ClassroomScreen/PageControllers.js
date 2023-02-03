99b435086c3cf6d072cfae88645ea708f34a51a8
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-03 09:31:01 +0900

    [Add] 페이지 번호 레이블 추가.

--- src/Screens/ClassroomScreen/PageControllers.js
+++ src/Screens/ClassroomScreen/PageControllers.js
@@ -9,15 +9,28 @@ const PageControllers = () => {
   return (
     <div className={`absolute top-[4vw] right-[15vw]`}>
       <div className={`flex flex-row`}>
-        <div>
+        <p
+          className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl cursor-pointer shadow-md`}
+          onClick={onClickFirstPage}
+        >
+          처음으로
+        </p>
+        <div className={`flex flex-row`}>
+          <img
+            src={"/assets/images/icons/left_page_button.svg"}
+            alt={`Left Page Button`}
+          />
           <p
-            className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl cursor-pointer shadow-md`}
-            onClick={onClickFirstPage}
+            className={`bg-white border-[1px] border-[#1AB9C5] rounded-3xl w-[120px] flex flex-row text-[28px] items-center justify-center`}
           >
-            처음으로
+            {currentPage}
+            <span className={`text-[20px] ml-1 relative top-[2px]`}>/19</span>
           </p>
+          <img
+            src={"/assets/images/icons/right_page_button.svg"}
+            alt={`Right Page Button`}
+          />
         </div>
-        <div></div>
       </div>
     </div>
   );
