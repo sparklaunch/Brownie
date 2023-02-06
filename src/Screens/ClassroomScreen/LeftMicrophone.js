fd80b702a47a46333305f820e2dc2c055250c18c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-06 10:03:06 +0900

    [Add] 리코더 구현.

--- src/Screens/ClassroomScreen/LeftMicrophone.js
+++ src/Screens/ClassroomScreen/LeftMicrophone.js
@@ -4,6 +4,32 @@ import { useRecoilState } from "recoil";
 const LeftMicrophone = () => {
   const [microphoneState, setMicrophoneState] =
     useRecoilState(microphoneStateAtom);
+  const recordVoice = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+        mediaRecorder.ondataavailable = (e) => {
+          const audioURL = window.URL.createObjectURL(e.data);
+          const audio = new Audio(audioURL);
+          audio.play();
+        };
+        mediaRecorder.onstop = () => {
+          stream.getTracks().forEach((track) => track.stop());
+        };
+      })
+      .catch((error) => {
+        if (error.message === "Requested device not found") {
+          alert("마이크를 찾을 수 없습니다.");
+        }
+      });
+  };
+  const onClickMicrophone = () => {
+    if (microphoneState === "idle") {
+      recordVoice();
+    }
+  };
   return (
     <div className={`absolute bottom-[-100px] left-[14vw]`}>
       {microphoneState === "idle" && (
@@ -16,6 +42,7 @@ const LeftMicrophone = () => {
       <img
         src={`/assets/images/icons/microphone_${microphoneState}.svg`}
         alt={"Microphone"}
+        onClick={onClickMicrophone}
         className={`relative left-0 w-[70px] h-[70px] ${
           microphoneState === "idle" ? "cursor-pointer" : ""
         }`}
