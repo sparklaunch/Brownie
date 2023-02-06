036af86c8417a52b7035031d0b291f4923ac5aa9
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-06 13:53:40 +0900

    [Add] 오른쪽 마이크 로직 추가.

--- src/Screens/ClassroomScreen/RightMicrophone.js
+++ src/Screens/ClassroomScreen/RightMicrophone.js
@@ -1,18 +1,49 @@
 import { useRecoilState } from "recoil";
 import microphoneStateAtom from "../../Stores/Classroom/microphoneState";
+import audioDurationAtom from "../../Stores/Classroom/audioDuration";
+import RecordingMicrophone from "./RecordingMicrophone";
 
 const RightMicrophone = () => {
   const [microphoneState, setMicrophoneState] =
     useRecoilState(microphoneStateAtom);
+  const [audioDuration, setAudioDuration] = useRecoilState(audioDurationAtom);
+  const recordVoice = async () => {
+    const device = await navigator.mediaDevices.getUserMedia({
+      audio: true
+    });
+    const recorder = new MediaRecorder(device);
+    recorder.start();
+    setMicrophoneState("recording");
+    recorder.ondataavailable = (event) => {
+      const audio = new Audio(URL.createObjectURL(event.data));
+      audio.play();
+      setMicrophoneState("idle");
+    };
+    setTimeout(() => {
+      recorder.stop();
+    }, audioDuration);
+  };
+  const onClickMicrophone = () => {
+    if (microphoneState === "idle") {
+      const sound = new Audio("/assets/audio/microphone_on.wav");
+      sound.play();
+      recordVoice();
+    }
+  };
   return (
     <div className={`absolute bottom-[-100px] right-[14vw]`}>
-      <img
-        src={`/assets/images/icons/microphone_${microphoneState}.svg`}
-        alt={"Microphone"}
-        className={`relative left-0 w-[70px] h-[70px] ${
-          microphoneState === "idle" ? "cursor-pointer" : ""
-        }`}
-      />
+      {microphoneState === "recording" ? (
+        <RecordingMicrophone />
+      ) : (
+        <img
+          src={`/assets/images/icons/microphone_${microphoneState}.svg`}
+          alt={"Microphone"}
+          onClick={onClickMicrophone}
+          className={`relative left-0 w-[70px] h-[70px] ${
+            microphoneState === "idle" ? "cursor-pointer" : ""
+          }`}
+        />
+      )}
     </div>
   );
 };
