32a7826f3cec7041919c630ccdd4afc7963e174d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-06 09:45:06 +0900

    [Add] 하울러 설치와 설정.

--- src/Screens/ClassroomScreen/AudioManager.js
+++ src/Screens/ClassroomScreen/AudioManager.js
@@ -1,22 +1,29 @@
-import ReactHowler from "react-howler";
 import { useRecoilState } from "recoil";
 import currentPageAtom from "../../Stores/Classroom/currentPage";
+import microphoneStateAtom from "../../Stores/Classroom/microphoneState";
+import { useEffect } from "react";
+import { Howl, Howler } from "howler";
 
 const AudioManager = () => {
+  const [microphoneState, setMicrophoneState] =
+    useRecoilState(microphoneStateAtom);
+  const onPlay = () => {
+    setMicrophoneState("disabled");
+  };
+  const onEnd = () => {
+    setMicrophoneState("idle");
+  };
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
-  if (currentPage === 3) {
-    return <ReactHowler src={`/assets/audio/pages/1-1-4.mp3`} playing={true} />;
-  }
-  if (currentPage >= 4 && currentPage <= 12) {
-    return (
-      <ReactHowler
-        src={`/assets/audio/pages/1-1-${currentPage}.mp3`}
-        playing={true}
-      />
-    );
-  } else {
-    return null;
-  }
+  useEffect(() => {
+    Howler.unload();
+    const howler = new Howl({
+      src: [`/assets/audio/pages/1-1-${currentPage}.mp3`],
+      onplay: onPlay,
+      onend: onEnd
+    });
+    howler.play();
+  }, [currentPage]);
+  return <></>;
 };
 
 export default AudioManager;
