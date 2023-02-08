2ba217b88c6c0b9583b617da228f38ca62f65366
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 09:46:55 +0900

    [Add] 섬네일 페이지들 추가.

--- src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
@@ -3,7 +3,7 @@ import ThumbnailPage from "./ThumbnailPage";
 const LeftEdgeThumbnail = () => {
   return (
     <div
-      className={`w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip shadow-lg mx-5`}
+      className={`w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip shadow-lg mx-3`}
     >
       <ThumbnailPage />
       <ThumbnailPage />
--- src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
@@ -1,5 +1,14 @@
+import ThumbnailPage from "./ThumbnailPage";
+
 const Thumbnail = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip shadow-lg mx-3`}
+    >
+      <ThumbnailPage />
+      <ThumbnailPage />
+    </div>
+  );
 };
 
 export default Thumbnail;
