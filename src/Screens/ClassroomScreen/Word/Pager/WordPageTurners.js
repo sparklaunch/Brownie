d5e21d4046dfcaed42c8f2ff08bca3635f0b0ef4
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 11:56:28 +0900

    [Add] 단어 데이터와 단어 페이지네이션 연동.

--- src/Screens/ClassroomScreen/Word/Pager/WordPageTurners.js
+++ src/Screens/ClassroomScreen/Word/Pager/WordPageTurners.js
@@ -2,14 +2,20 @@ import LeftPageTurner from "./WordLeftPageTurner";
 import RightPageTurner from "./WordRightPageTurner";
 import { useRecoilState } from "recoil";
 import currentWordPageAtom from "../../../../Stores/Classroom/Word/currentWordPage";
+import data from "../../../../data.json";
+import { useParams } from "react-router-dom";
 
 const WordPageTurners = () => {
+  const { level } = useParams();
+  const dataString = JSON.stringify(data);
+  const dataObject = JSON.parse(dataString);
+  const words = dataObject.find((item) => item.level === level).words;
   const [currentWordPage, setCurrentWordPage] =
     useRecoilState(currentWordPageAtom);
   return (
     <div>
       <LeftPageTurner visible={currentWordPage !== 1} />
-      <RightPageTurner visible={currentWordPage !== 11} />
+      <RightPageTurner visible={currentWordPage !== words.length} />
     </div>
   );
 };
--- src/Screens/ClassroomScreen/Word/Pager/WordRightPageTurner.js
+++ src/Screens/ClassroomScreen/Word/Pager/WordRightPageTurner.js
@@ -1,11 +1,17 @@
 import { useRecoilState } from "recoil";
 import currentWordPageAtom from "../../../../Stores/Classroom/Word/currentWordPage";
+import data from "../../../../data.json";
+import { useParams } from "react-router-dom";
 
 const WordRightPageTurner = ({ visible }) => {
+  const { level } = useParams();
+  const dataString = JSON.stringify(data);
+  const dataObject = JSON.parse(dataString);
+  const words = dataObject.find((item) => item.level === level).words;
   const [currentWordPage, setCurrentWordPage] =
     useRecoilState(currentWordPageAtom);
   const onClickRightPageTurner = () => {
-    setCurrentWordPage(Math.min(currentWordPage + 1, 11));
+    setCurrentWordPage(Math.min(currentWordPage + 1, words.length));
   };
   if (visible) {
     return (
