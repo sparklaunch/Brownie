0f1824a7974a2f92ca20efaf598b2a218c85ee1f
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 12:00:46 +0900

    [Add] 워드 모드 페이지 변할 때마다 마이크 상태 아이들로 돌아가게 수정.

--- src/Screens/ClassroomScreen/Word/Microphone/Microphone.js
+++ src/Screens/ClassroomScreen/Word/Microphone/Microphone.js
@@ -3,11 +3,18 @@ import { useRecoilState } from "recoil";
 import wordMicrophoneStateAtom from "../../../../Stores/Classroom/Word/wordMicrophoneState";
 import RecordingMicrophone from "./RecordingMicrophone";
 import DisabledMicrophone from "./DisabledMicrophone";
+import currentWordPageAtom from "../../../../Stores/Classroom/Word/currentWordPage";
+import { useEffect } from "react";
 
 const Microphone = () => {
+  const [currentWordPage, setCurrentWordPage] =
+    useRecoilState(currentWordPageAtom);
   const [wordMicrophoneState, setWordMicrophoneState] = useRecoilState(
     wordMicrophoneStateAtom
   );
+  useEffect(() => {
+    setWordMicrophoneState("idle");
+  }, [currentWordPage]);
   switch (wordMicrophoneState) {
     case "idle":
       return <IdleMicrophone />;
