d3843c464a20a1ab151d54b45f5f9e31c539c729
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 11:36:15 +0900

    [Add] 마이크 상태에 따른 컴포넌트 분기 추가.

---
+++ src/Screens/ClassroomScreen/Word/Microphone/IdleMicrophone.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+const IdleMicrophone = () => {
+  const OuterCircle = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    transform: scale(1.2);
+    background-color: #1ab9c5;
+    animation: fadeOut 1.3s infinite;
+    @keyframes fadeOut {
+      0% {
+        opacity: 0.3;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
+  `;
+  const InnerCircle = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    transform: scale(1.1);
+    background-color: #1ab9c5;
+    animation: fadeOut 1.3s infinite;
+    @keyframes fadeOut {
+      0% {
+        opacity: 0.3;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
+  `;
+  const Wave = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    transform: scale(1);
+    border: 5px solid #1ab9c5;
+    animation: wave 1.3s infinite;
+    @keyframes wave {
+      0% {
+        transform: scale(1);
+        opacity: 1;
+      }
+      100% {
+        transform: scale(1.4);
+        opacity: 0;
+      }
+    }
+  `;
+  return (
+    <div className={`absolute left-[50%] translate-x-[-50%] bottom-5`}>
+      <div>
+        <OuterCircle />
+        <InnerCircle />
+        <Wave />
+      </div>
+      <img
+        src={`/assets/images/icons/microphone_idle.svg`}
+        alt={"Microphone"}
+        className={`relative left-0 w-[70px] h-[70px] cursor-pointer`}
+      />
+    </div>
+  );
+};
+
+export default IdleMicrophone;
