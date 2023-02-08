86a5e614b6b1783b0a944f9ec0490ab621c667d9
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 09:45:33 +0900

    [Update] 섬네일 레이아웃 수정.

--- src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
@@ -3,7 +3,7 @@ import ThumbnailPage from "./ThumbnailPage";
 const LeftEdgeThumbnail = () => {
   return (
     <div
-      className={`w-[200px] h-[150px] grid grid-cols-2 rounded-lg overflow-clip shadow-lg mx-5`}
+      className={`w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip shadow-lg mx-5`}
     >
       <ThumbnailPage />
       <ThumbnailPage />
