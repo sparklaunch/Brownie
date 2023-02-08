5c144c39d74ca8b83b52003e9a68f68e715763b5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 11:59:26 +0900

    [Add] 페이지 변할 때마다 마이크 상태 아이들로 돌아가게 수정.

--- src/Screens/ClassroomScreen/Story/Microphone/Microphones.js
+++ src/Screens/ClassroomScreen/Story/Microphone/Microphones.js
@@ -2,9 +2,16 @@ import { useRecoilState } from "recoil";
 import LeftMicrophone from "./Left/LeftMicrophone";
 import RightMicrophone from "./Right/RightMicrophone";
 import currentPageAtom from "../../../../Stores/Classroom/Story/currentPage";
+import { useEffect } from "react";
+import microphoneStateAtom from "../../../../Stores/Classroom/Story/microphoneState";
 
 const Microphones = () => {
+  const [microphoneState, setMicrophoneState] =
+    useRecoilState(microphoneStateAtom);
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  useEffect(() => {
+    setMicrophoneState("idle");
+  }, [currentPage]);
   return (
     <div className={`absolute w-[70vw] h-[46vw]`}>
       <LeftMicrophone visible={currentPage !== 0} />
