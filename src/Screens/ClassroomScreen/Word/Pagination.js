dd7dff3e8a0a623509926a82edacd9e2dc67d5f5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-08 11:26:17 +0900

    [Add] data.json과 워드 모드 연동.

--- src/Screens/ClassroomScreen/Word/Pagination.js
+++ src/Screens/ClassroomScreen/Word/Pagination.js
@@ -1,14 +1,20 @@
 import { useRecoilState } from "recoil";
 import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
+import { useParams } from "react-router-dom";
+import data from "../../../data.json";
 
 const Pagination = () => {
+  const { level } = useParams();
+  const dataString = JSON.stringify(data);
+  const dataObject = JSON.parse(dataString);
+  const words = dataObject.find((book) => book.level === level).words;
   const [currentWordPage, setCurrentWordPage] =
     useRecoilState(currentWordPageAtom);
   return (
     <div className={`absolute top-[36px] right-[36px]`}>
       <p className={`text-[28px] text-[#515151] font-bold tracking-wide`}>
         {currentWordPage}
-        <span className={`text-[20px] text-gray-400`}>/11</span>
+        <span className={`text-[20px] text-gray-400`}>/{words.length}</span>
       </p>
     </div>
   );
--- src/Screens/ClassroomScreen/Word/Word.js
+++ src/Screens/ClassroomScreen/Word/Word.js
@@ -1,4 +1,15 @@
+import { useParams } from "react-router-dom";
+import data from "../../../data.json";
+import { useRecoilState } from "recoil";
+import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
+
 const Word = () => {
+  const [currentWordPage, setCurrentWordPage] =
+    useRecoilState(currentWordPageAtom);
+  const { level } = useParams();
+  const dataString = JSON.stringify(data);
+  const dataObject = JSON.parse(dataString);
+  const words = dataObject.find((book) => book.level === level).words;
   return (
     <div className={`h-full flex flex-row justify-center items-center`}>
       <img
@@ -6,7 +17,9 @@ const Word = () => {
         alt={"Megaphone"}
         className={`cursor-pointer`}
       />
-      <p className={`text-[120px] font-[900] ml-[24px]`}>ant</p>
+      <p className={`text-[120px] font-[900] ml-[24px]`}>
+        {words[currentWordPage - 1]}
+      </p>
     </div>
   );
 };
