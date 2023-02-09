b30980c6336ba0b3ff443d38a6ce30cb1b272a9c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 16:32:04 +0900

    [Add] 굳 원숭이 추가.

--- src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreen.js
@@ -1,5 +1,15 @@
 const GoodScreen = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#FFC767] p-8`}
+    >
+      <img
+        src={`/assets/images/good_monkey.png`}
+        alt={`Good Monkey`}
+        className={`absolute top-[50%] translate-y-[-50%] left-0 h-[200px] w-[185px]`}
+      />
+    </div>
+  );
 };
 
 export default GoodScreen;
