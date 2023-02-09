30017bcc36758babb3cd0e63b4a365bbea7c1cd1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 17:39:46 +0900

    [Add] 필드 스타와 엠프티 스타 추가.

--- src/Screens/ClassroomScreen/Story/Results/Score/ScoreStar.js
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScoreStar.js
@@ -1,5 +1,21 @@
 const ScoreStar = ({ filled }) => {
-  return <div></div>;
+  if (filled) {
+    return (
+      <img
+        src={`/assets/images/star_filled.svg`}
+        alt={`Filled Star`}
+        className={`h-[26px] w-[26px]`}
+      />
+    );
+  } else {
+    return (
+      <img
+        src={`/assets/images/star_empty.svg`}
+        alt={`Empty Star`}
+        className={`h-[26px] w-[26px]`}
+      />
+    );
+  }
 };
 
 export default ScoreStar;
