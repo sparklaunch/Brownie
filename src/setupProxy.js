b9c050dab04a480148b5bab7c9b064706e48dd1f
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 14:04:39 +0900

    [Update] 다중 프록시 설정.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,17 +2,18 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
-    createProxyMiddleware({
+    createProxyMiddleware("/api", {
       target: "http://43.201.129.192",
       changeOrigin: true
     })
   );
   app.use(
-    "/ela",
-    createProxyMiddleware({
-      target: "https://api.elasolution.com",
-      changeOrigin: true
+    createProxyMiddleware("/ela", {
+      target: "https://api.elasolution.com/",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/ela": ""
+      }
     })
   );
 };
