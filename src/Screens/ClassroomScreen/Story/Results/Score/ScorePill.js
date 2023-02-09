f0bd16f42f908cbc56f35eedaded8add6d314d7f
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 17:46:17 +0900

    [Add] 스코어필 보더 추가.

--- src/Screens/ClassroomScreen/Story/Results/Score/ScorePill.js
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScorePill.js
@@ -1,11 +1,28 @@
 import ScoreStars from "./ScoreStars";
 import ScoreText from "./ScoreText";
+import { useRecoilState } from "recoil";
+import totalScoreAtom from "../../../../../Stores/Classroom/Story/totalScore";
 
 const ScorePill = () => {
+  const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
+  let borderColor;
+  if (totalScore.score >= 50) {
+    borderColor = `border-[#15B58F]`;
+  } else if (totalScore.score >= 20) {
+    borderColor = `border-[#FF8200]`;
+  } else {
+    borderColor = `border-[#FF2442]`;
+  }
   return (
-    <div>
-      <ScoreStars />
-      <ScoreText />
+    <div
+      className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+    >
+      <div
+        className={`flex flex-row items-center justify-center bg-white rounded-full px-8 py-1 border-2 shadow-lg ${borderColor}`}
+      >
+        <ScoreStars />
+        <ScoreText />
+      </div>
     </div>
   );
 };
--- src/Screens/ClassroomScreen/Story/Results/Score/ScoreText.js
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScoreText.js
@@ -5,12 +5,16 @@ const ScoreText = () => {
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
   if (totalScore.score >= 50) {
     return (
-      <p className={`text-[36px] font-[Jua] text-[#15B58F]`}>Excellent!</p>
+      <p className={`text-[36px] font-[Jua] text-[#15B58F] ml-3`}>Excellent!</p>
     );
   } else if (totalScore.score >= 20) {
-    return <p className={`text-[36px] font-[Jua] text-[#FF8200]`}>Good!</p>;
+    return (
+      <p className={`text-[36px] font-[Jua] text-[#FF8200] ml-3`}>Good!</p>
+    );
   } else {
-    return <p className={`text-[36px] font-[Jua] text-[#FF2442]`}>Nice Try!</p>;
+    return (
+      <p className={`text-[36px] font-[Jua] text-[#FF2442] ml-3`}>Nice Try!</p>
+    );
   }
 };
 
