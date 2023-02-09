fe47d0d8520274af2412c5a03115dbc6afac0bcb
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 17:27:59 +0900

    [Add] 결과 화면 효과음 추가.

--- src/Screens/ClassroomScreen/Story/Results/ResultsScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/ResultsScreen.js
@@ -3,9 +3,13 @@ import totalScoreAtom from "../../../../Stores/Classroom/Story/totalScore";
 import ExcellentScreen from "./ExcellentScreen/ExcellentScreen";
 import GoodScreen from "./GoodScreen/GoodScreen";
 import NiceTryScreen from "./NiceTryScreen/NiceTryScreen";
+import { useEffect } from "react";
 
 const ResultsScreen = () => {
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
+  useEffect(() => {
+    new Audio(`/assets/audio/results_shown.mp3`).play();
+  }, []);
   if (totalScore.score >= 50) {
     return <ExcellentScreen />;
   } else if (totalScore.score >= 20) {
