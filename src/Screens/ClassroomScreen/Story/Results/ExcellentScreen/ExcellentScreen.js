7606ff5ac5a5e7829e846260ef7b2954835733e1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 16:30:39 +0900

    [Add] 엑셀런트 박쥐 추가.

--- src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreen.js
@@ -1,5 +1,15 @@
 const ExcellentScreen = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#80E6AC] p-8`}
+    >
+      <img
+        src={`/assets/images/excellent_bat.png`}
+        alt={`Excellent bat`}
+        className={`absolute top-[50%] translate-y-[-50%] right-0 h-[200px] w-[300px]`}
+      />
+    </div>
+  );
 };
 
 export default ExcellentScreen;
