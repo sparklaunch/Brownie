79565f00db6b338a71d7d361b2c2b8312972030c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 16:33:23 +0900

    [Add] 나이스 츄라이 개구리 추가.

--- src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreen.js
@@ -1,5 +1,15 @@
 const NiceTryScreen = () => {
-  return <div></div>;
+  return (
+    <div
+      className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#FF92AC] p-8`}
+    >
+      <img
+        src={`/assets/images/nicetry_frog.png`}
+        alt={`Nice Try Frog`}
+        className={`absolute top-[50%] translate-y-[-50%] right-0 h-[200px] w-[215px]`}
+      />
+    </div>
+  );
 };
 
 export default NiceTryScreen;
