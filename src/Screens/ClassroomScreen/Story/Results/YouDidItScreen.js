b5d87bc678277487261083dc86131b321b5622f6
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-09 09:22:00 +0900

    [Add] 유디릿 텍스트 추가.

--- src/Screens/ClassroomScreen/Story/Results/YouDidItScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/YouDidItScreen.js
@@ -1,9 +1,11 @@
+import YouDidItText from "./YouDidItText";
+
 const YouDidItScreen = () => {
   return (
     <div
       className={`absolute top-0 left-0 bottom-0 right-0 bg-black opacity-50`}
     >
-      <YouDidItScreen />
+      <YouDidItText />
     </div>
   );
 };
