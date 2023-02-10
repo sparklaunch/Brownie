947de4862717026f6562d61d8e24d6077d95628b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-10 09:16:29 +0900

    [Add] 유디릿 음원 추가.

--- src/Screens/ClassroomScreen/Story/Results/YouDidItScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/YouDidItScreen.js
@@ -2,8 +2,12 @@ import YouDidItText from "./YouDidItText";
 import ResultsBackDrop from "./ResultsBackDrop";
 import ReadingMonkey from "./ReadingMonkey";
 import Confetti from "./Confetti";
+import { useEffect } from "react";
 
 const YouDidItScreen = () => {
+  useEffect(() => {
+    new Audio(`/assets/audio/you_did_it.mp3`).play();
+  }, []);
   return (
     <div className={`absolute top-0 left-0 bottom-0 right-0`}>
       <ResultsBackDrop />
