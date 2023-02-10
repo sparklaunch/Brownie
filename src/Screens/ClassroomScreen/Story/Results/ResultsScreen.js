02b5291269e2adce6435659c85c03d535952d85b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-10 09:02:36 +0900

    [Add] 결과 바 2초 뒤에 사라지게 수정.

--- src/Screens/ClassroomScreen/Story/Results/ResultsScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/ResultsScreen.js
@@ -4,18 +4,29 @@ import ExcellentScreen from "./ExcellentScreen/ExcellentScreen";
 import GoodScreen from "./GoodScreen/GoodScreen";
 import NiceTryScreen from "./NiceTryScreen/NiceTryScreen";
 import { useEffect } from "react";
+import resultsScreenShownAtom from "../../../../Stores/Classroom/Story/resultsScreenShown";
 
 const ResultsScreen = () => {
+  const [resultsScreenShown, setResultsScreenShown] = useRecoilState(
+    resultsScreenShownAtom
+  );
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
   useEffect(() => {
     new Audio(`/assets/audio/results_shown.mp3`).play();
+    setTimeout(() => {
+      setResultsScreenShown(false);
+    }, 2000);
   }, []);
-  if (totalScore.score >= 50) {
-    return <ExcellentScreen />;
-  } else if (totalScore.score >= 20) {
-    return <GoodScreen />;
+  if (resultsScreenShown) {
+    if (totalScore.score >= 50) {
+      return <ExcellentScreen />;
+    } else if (totalScore.score >= 20) {
+      return <GoodScreen />;
+    } else {
+      return <NiceTryScreen />;
+    }
   } else {
-    return <NiceTryScreen />;
+    return <></>;
   }
 };
 
