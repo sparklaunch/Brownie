0bf0cc93f669c6d673968a5c506fcad69707cfe8
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-10 09:14:30 +0900

    [Add] 다시 하기와 내 목소리 다시 들어보기 버튼 핸들러 추가.

--- src/Screens/ClassroomScreen/Story/Microphone/Left/LeftButtons.js
+++ src/Screens/ClassroomScreen/Story/Microphone/Left/LeftButtons.js
@@ -1,12 +1,24 @@
 const LeftButtons = () => {
+  const onClickRetry = () => {
+    console.log("Retry");
+  };
+  const onClickMyVoice = () => {
+    console.log("My Voice");
+  };
   return (
     <div className={`flex flex-row`}>
       <img
         src={`/assets/images/retry_button.svg`}
         alt={`Retry Button`}
-        className={`mr-3`}
+        className={`mr-3 cursor-pointer`}
+        onClick={onClickRetry}
       />
-      <img src={`/assets/images/my_voice.svg`} alt={`My Voice Button`} />
+      <img
+        src={`/assets/images/my_voice.svg`}
+        alt={`My Voice Button`}
+        className={`cursor-pointer`}
+        onClick={onClickMyVoice}
+      />
     </div>
   );
 };
