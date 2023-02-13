cfe05df3068518507d89c0359072cc80223a512d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-13 15:03:28 +0900

    [Add] 회원 정보 비밀번호 입력란에 공백 입력할 수 없게 수정.

--- src/Screens/MyPageScreen/AccountInformation/PasswordField.js
+++ src/Screens/MyPageScreen/AccountInformation/PasswordField.js
@@ -4,13 +4,16 @@ import passwordAtom from "../../../Stores/Auth/password";
 
 const PasswordField = () => {
   const [password, setPassword] = useRecoilState(passwordAtom);
+  const onChangePassword = (event) => {
+    setPassword(event.target.value.replace(/\s/g, ""));
+  };
   return (
     <div className={`flex flex-row justify-center mb-6`}>
       <TextField
         variant={"outlined"}
         label={"비밀번호"}
         value={password}
-        onChange={(event) => setPassword(event.target.value)}
+        onChange={onChangePassword}
         type={"password"}
         size={"small"}
         sx={{
