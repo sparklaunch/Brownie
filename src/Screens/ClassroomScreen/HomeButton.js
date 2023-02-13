459e3e00ebe8895d00beae06bfac568ef419aaae
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-14 08:25:02 +0900

    [Add] 홈 버튼 클릭 시 음원 재생 멈추게 수정.

--- src/Screens/ClassroomScreen/HomeButton.js
+++ src/Screens/ClassroomScreen/HomeButton.js
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import currentPageAtom from "../../Stores/Classroom/Story/currentPage";
 import modeAtom from "../../Stores/Classroom/mode";
+import { Howler } from "howler";
 
 const HomeButton = () => {
   const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
   const [mode, setMode] = useRecoilState(modeAtom);
   const onClickHome = () => {
+    Howler.unload();
     setMode("story");
     setCurrentPage(0);
     navigate("/");
