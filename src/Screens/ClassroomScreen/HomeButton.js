c1c9ea12761215320fdc6fe59ee80b55b49d4369
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-14 08:27:38 +0900

    [Add] 홈 버튼 클릭 시 워드 페이지도 1로 초기화되게 수정.

--- src/Screens/ClassroomScreen/HomeButton.js
+++ src/Screens/ClassroomScreen/HomeButton.js
@@ -3,15 +3,19 @@ import { useRecoilState } from "recoil";
 import currentPageAtom from "../../Stores/Classroom/Story/currentPage";
 import modeAtom from "../../Stores/Classroom/mode";
 import { Howler } from "howler";
+import currentWordPageAtom from "../../Stores/Classroom/Word/currentWordPage";
 
 const HomeButton = () => {
   const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  const [currentWordPage, setCurrentWordPage] =
+    useRecoilState(currentWordPageAtom);
   const [mode, setMode] = useRecoilState(modeAtom);
   const onClickHome = () => {
     Howler.unload();
     setMode("story");
     setCurrentPage(0);
+    setCurrentWordPage(1);
     navigate("/");
   };
   return (
