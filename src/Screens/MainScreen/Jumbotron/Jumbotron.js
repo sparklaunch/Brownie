4b3d6d4f8d60e1a4e8241f82abe2fec3f3314376
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-13 14:37:15 +0900

    [Add] 일러스트 점보트론 추가.

--- src/Screens/MainScreen/Jumbotron/Jumbotron.js
+++ src/Screens/MainScreen/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import JumbotronForm from "./JumbotronForm";
 const Jumbotron = () => {
   return (
     <div
-      className={`h-[540px] bg-[#47C7D2] flex flex-row justify-center items-center`}
+      className={`h-[540px] flex bg-jumbotron flex-row justify-center items-center`}
     >
       <div className={`w-[1200px] flex flex-row justify-between`}>
         <JumbotronTitle />
