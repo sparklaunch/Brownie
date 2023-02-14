d1b974540c7230a44d4f7ef8fcdfa18f693cd289
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-14 11:23:50 +0900

    [Add] 페이지 클릭 가능하게 수정.

--- src/Screens/ClassroomScreen/Story/LeftPage.js
+++ src/Screens/ClassroomScreen/Story/LeftPage.js
@@ -1,6 +1,7 @@
 const LeftPage = ({ fileName, isEmpty }) => {
+  const onClickLeftPage = () => {};
   return (
-    <div className={`relative`}>
+    <div className={`relative z-[1] cursor-pointer`} onClick={onClickLeftPage}>
       {isEmpty || <img src={fileName} alt={"Left Page"} loading={"lazy"} />}
       <div
         className={`absolute right-0 top-0 h-full w-10 bg-gradient-to-r from-transparent to-black opacity-50`}
--- src/Screens/ClassroomScreen/Story/RightPage.js
+++ src/Screens/ClassroomScreen/Story/RightPage.js
@@ -1,6 +1,7 @@
 const RightPage = ({ fileName, isEmpty }) => {
+  const onClickRightPage = () => {};
   return (
-    <div className={`relative`}>
+    <div className={`relative z-[1] cursor-pointer`} onClick={onClickRightPage}>
       {isEmpty || <img src={fileName} alt={"Right Page"} loading={"lazy"} />}
       <div
         className={`absolute left-0 top-0 h-full w-10 bg-gradient-to-r from-black opacity-50`}
