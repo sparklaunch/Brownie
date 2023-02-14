a65fe9c170583d0fadeb6e9f115b7e3db62b3528
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-14 09:05:02 +0900

    [Add] 점보트론 로그인 기능 구현.

--- src/Screens/MainScreen/Jumbotron/FormFields.js
+++ src/Screens/MainScreen/Jumbotron/FormFields.js
@@ -3,13 +3,56 @@ import { useRecoilState } from "recoil";
 import idAtom from "../../../Stores/Auth/id";
 import passwordAtom from "../../../Stores/Auth/password";
 import autoLogInEnabledAtom from "../../../Stores/Auth/autoLogInEnabled";
+import axios from "axios";
 
 const FormFields = () => {
   const [id, setID] = useRecoilState(idAtom);
   const [password, setPassword] = useRecoilState(passwordAtom);
   const [autoLogInEnabled, setAutoLogInEnabled] =
     useRecoilState(autoLogInEnabledAtom);
-  const onLogInButtonClicked = () => {};
+  const clearAllFields = () => {
+    setID("");
+    setPassword("");
+  };
+  const signIn = async () => {
+    try {
+      const response = await axios.post(
+        `/api/ap002?id=${id}&pwd=${password}`,
+        "",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      );
+      const stringResponse = JSON.stringify(response, null, 2);
+      console.log(stringResponse);
+      if (response.data.resultCode === "100") {
+        alert("로그인에 성공하였습니다.");
+        clearAllFields();
+        sessionStorage.setItem("userNumber", response.data.user_no);
+      } else if (response.data.resultCode === "200") {
+        alert("비밀번호가 일치하지 않습니다.");
+      } else if (response.data.resultCode === "900") {
+        alert("로그인에 실패했습니다.");
+      } else {
+        alert("서버 에러.");
+      }
+    } catch (error) {
+      const errorString = JSON.stringify(error, null, 2);
+      console.log(errorString);
+    }
+  };
+  const onLogInButtonClicked = () => {
+    if (id.length === 0) {
+      alert("아이디를 입력해주세요.");
+    } else if (password.length === 0) {
+      alert("비밀번호를 입력해주세요.");
+    } else {
+      signIn();
+    }
+  };
   return (
     <div>
       <TextField
