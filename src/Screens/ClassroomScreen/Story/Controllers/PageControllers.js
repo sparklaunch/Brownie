25f9d9a279faddceb263b0ce0274428b76fd7911
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-16 06:41:07 +0900

    [Fix] 녹음 중이거나, 로딩 중일 때 우상단 컨트롤러 비활성화되게 수정.

--- src/Screens/ClassroomScreen/Story/Controllers/PageControllers.js
+++ src/Screens/ClassroomScreen/Story/Controllers/PageControllers.js
@@ -2,29 +2,59 @@ import { useRecoilState } from "recoil";
 import currentPageAtom from "../../../../Stores/Classroom/Story/currentPage";
 import LeftPageAngle from "./LeftPageAngle";
 import RightPageAngle from "./RightPageAngle";
+import centralMicrophoneStateAtom from "../../../../Stores/Classroom/Story/Microphones/centralMicrophoneState";
 
 const PageControllers = () => {
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  const [centralMicrophoneState, setCentralMicrophoneState] = useRecoilState(
+    centralMicrophoneStateAtom
+  );
   const onClickFirstPage = () => {
-    setCurrentPage(0);
+    if (
+      centralMicrophoneState !== "invisible" &&
+      centralMicrophoneState !== "loading"
+    ) {
+      setCurrentPage(0);
+    }
   };
   const onLeftPageButtonClick = () => {
-    setCurrentPage(Math.max(currentPage - 2, 0));
+    if (
+      centralMicrophoneState !== "invisible" &&
+      centralMicrophoneState !== "loading"
+    ) {
+      setCurrentPage(Math.max(currentPage - 2, 0));
+    }
   };
   const onRightPageButtonClick = () => {
-    setCurrentPage(Math.min(currentPage + 2, 10));
+    if (
+      centralMicrophoneState !== "invisible" &&
+      centralMicrophoneState !== "loading"
+    ) {
+      setCurrentPage(Math.min(currentPage + 2, 10));
+    }
   };
   return (
     <div className={`flex flex-row`}>
       <p
-        className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl cursor-pointer shadow-md mr-3`}
+        className={`font-[Jua] text-[20px] text-white px-5 py-2 bg-[#1AB9C5] rounded-3xl ${
+          centralMicrophoneState !== "invisible" &&
+          centralMicrophoneState !== "loading"
+            ? `cursor-pointer`
+            : ``
+        } shadow-md mr-3`}
         onClick={onClickFirstPage}
       >
         처음으로
       </p>
       <div className={`flex flex-row`}>
         <div onClick={onLeftPageButtonClick}>
-          <LeftPageAngle disabled={currentPage <= 0} />
+          <LeftPageAngle
+            disabled={
+              currentPage <= 0 ||
+              centralMicrophoneState === "invisible" ||
+              centralMicrophoneState === "loading"
+            }
+          />
         </div>
         <p
           className={`bg-white border-[1px] border-[#1AB9C5] rounded-3xl w-[120px] flex flex-row text-[28px] items-center justify-center`}
@@ -33,7 +63,13 @@ const PageControllers = () => {
           <span className={`text-[20px] ml-1 relative top-[2px]`}>/10</span>
         </p>
         <div onClick={onRightPageButtonClick}>
-          <RightPageAngle disabled={currentPage >= 10} />
+          <RightPageAngle
+            disabled={
+              currentPage >= 10 ||
+              centralMicrophoneState === "invisible" ||
+              centralMicrophoneState === "loading"
+            }
+          />
         </div>
       </div>
     </div>
