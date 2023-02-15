ed767a628f39ccc1726916a06dcf94c46c8b0f9d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-15 09:38:53 +0900

    [Add] 이미지 화면에서 확성기 클릭 시 TTS 추가.

--- src/Screens/ClassroomScreen/Word/WordImage.js
+++ src/Screens/ClassroomScreen/Word/WordImage.js
@@ -2,12 +2,19 @@ import { useParams } from "react-router-dom";
 import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
 import { useRecoilState } from "recoil";
 import useData from "../../../Hooks/useData";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 const WordImage = () => {
   const words = useData("words");
   const { level } = useParams();
   const [currentWordPage, setCurrentWordPage] =
     useRecoilState(currentWordPageAtom);
+  const { speak } = useSpeechSynthesis();
+  const onClickMegaphone = () => {
+    speak({
+      text: words[currentWordPage - 1]
+    });
+  };
   return (
     <div
       className={`w-[76%] h-[76%] border-[24px] border-[#FFD53D] rounded-2xl flex flex-col justify-center items-center relative`}
@@ -23,7 +30,8 @@ const WordImage = () => {
         <img
           src={`/assets/images/icons/megaphone_button.svg`}
           alt="Megaphone Button"
-          className={`mr-3`}
+          className={`mr-3 cursor-pointer`}
+          onClick={onClickMegaphone}
         />
         <p className={`text-[#15B58F] font-black text-[60px]`}>
           {words[currentWordPage - 1]}
