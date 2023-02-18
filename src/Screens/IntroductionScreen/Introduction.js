1fbc6bcaf0d9bf429423a5293e72f2660d002be1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 16:22:05 +0900

    [Add] 푸터 추가.

--- src/Screens/IntroductionScreen/Introduction.js
+++ src/Screens/IntroductionScreen/Introduction.js
@@ -3,6 +3,7 @@ import IntroductionContent from "./IntroductionContent/IntroductionContent";
 import IntroductionPreviews from "./IntroductionPreviews/IntroductionPreviews";
 import IntroductionCatalog from "./IntroductionCatalog/IntroductionCatalog";
 import IntroductionTry from "./IntroductionTry/IntroductionTry";
+import Footer from "../MainScreen/Footer/Footer";
 
 const Introduction = () => {
   return (
@@ -12,6 +13,7 @@ const Introduction = () => {
       <IntroductionPreviews />
       <IntroductionCatalog />
       <IntroductionTry />
+      <Footer />
     </div>
   );
 };
