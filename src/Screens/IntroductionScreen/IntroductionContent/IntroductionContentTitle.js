82c183e7664a85d2697babe0f98008ced329fd92
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:08:44 +0900

    [Add] 인트로덕션 컨텐츠 타이틀 텍스트 추가.

--- src/Screens/IntroductionScreen/IntroductionContent/IntroductionContentTitle.js
+++ src/Screens/IntroductionScreen/IntroductionContent/IntroductionContentTitle.js
@@ -1,5 +1,13 @@
 const IntroductionContentTitle = () => {
-  return <div></div>;
+  return (
+    <div className={`flex flex-col items-center`}>
+      <p className={`font-[Jua] text-[44px] mb-[16px]`}>AI 스피킹이란?</p>
+      <p className={`text-[20px] text-[#515151]`}>
+        영어를 읽고 말하기 학습을 할 수 있는
+      </p>
+      <p className={`text-[20px] text-[#515151]`}>온라인 스피킹 프로그램</p>
+    </div>
+  );
 };
 
 export default IntroductionContentTitle;
