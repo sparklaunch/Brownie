a88504bbc27f3b04ceefd4615639e8eb8dc867c4
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 16:04:50 +0900

    [Add] 무료 체험 버튼 추가.

--- src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
@@ -1,10 +1,12 @@
 import IntroductionTryTitle from "./IntroductionTryTitle";
+import IntroductionTryButton from "./IntroductionTryButton";
 
 const IntroductionTry = () => {
   return (
     <div className={`flex flex-row justify-center bg-[#1AC573] p-[100px]`}>
       <div className={`w-[1200px]`}>
         <IntroductionTryTitle />
+        <IntroductionTryButton />
       </div>
     </div>
   );
---
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTryButton.js
@@ -0,0 +1,11 @@
+const IntroductionTryButton = () => {
+  return (
+    <p
+      className={`inline-block text-white font-[Jua] text-[24px] px-[100px] py-[20px] border-2 border-white rounded-lg`}
+    >
+      무료 체험
+    </p>
+  );
+};
+
+export default IntroductionTryButton;
