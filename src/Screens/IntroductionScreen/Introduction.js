62f5cb7afc141c864e1ae740afd794e44822235d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:21:13 +0900

    [Add] 프로그램 미리보기 추가.

--- src/Screens/IntroductionScreen/Introduction.js
+++ src/Screens/IntroductionScreen/Introduction.js
@@ -1,11 +1,13 @@
 import IntroductionHeader from "./IntroductionHeader";
 import IntroductionContent from "./IntroductionContent/IntroductionContent";
+import IntroductionPreviews from "./IntroductionPreviews/IntroductionPreviews";
 
 const Introduction = () => {
   return (
     <div>
       <IntroductionHeader />
       <IntroductionContent />
+      <IntroductionPreviews />
     </div>
   );
 };
---
+++ src/Screens/IntroductionScreen/IntroductionPreviews/IntroductionPreviews.js
@@ -0,0 +1,9 @@
+const IntroductionPreviews = () => {
+  return (
+    <div className={`flex flex-row justify-center p-[140px]`}>
+      <div className={`w-[1200px] flex flex-row justify-between`}></div>
+    </div>
+  );
+};
+
+export default IntroductionPreviews;
