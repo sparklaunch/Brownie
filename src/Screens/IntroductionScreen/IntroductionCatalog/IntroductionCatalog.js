25944f19a8597a784203970c9db126f75d08a280
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:34:04 +0900

    [Add] 소개 페이지 카탈로그 타이틀 추가.

--- src/Screens/IntroductionScreen/IntroductionCatalog/IntroductionCatalog.js
+++ src/Screens/IntroductionScreen/IntroductionCatalog/IntroductionCatalog.js
@@ -1,9 +1,13 @@
+import IntroductionCatalogTitle from "./IntroductionCatalogTitle";
+
 const IntroductionCatalog = () => {
   return (
     <div
       className={`flex flex-row justify-center p-[120px] bg-[#F4FEFF] border-t-[1px] border-b-[1px] border-[#1AB9C5]`}
     >
-      <div className={`w-[1200px] flex flex-col items-center`}></div>
+      <div className={`w-[1200px] flex flex-col items-center`}>
+        <IntroductionCatalogTitle />
+      </div>
     </div>
   );
 };
---
+++ src/Screens/IntroductionScreen/IntroductionCatalog/IntroductionCatalogTitle.js
@@ -0,0 +1,12 @@
+const IntroductionCatalogTitle = () => {
+  return (
+    <div className={`flex flex-col items-center`}>
+      <p className={`font-[Jua] text-[44px]`}>
+        파닉스 전용 교재 읽기 연습을 통한
+      </p>
+      <p className={`font-[Jua] text-[44px]`}>파닉스 완성</p>
+    </div>
+  );
+};
+
+export default IntroductionCatalogTitle;
