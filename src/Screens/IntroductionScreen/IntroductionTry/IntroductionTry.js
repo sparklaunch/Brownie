775ef3fd6f0e2ab4ca8ea2fc0caab41674bc0d48
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 16:01:55 +0900

    [Add] 무료 체험 섹션 타이틀 추가.

--- src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
@@ -1,7 +1,11 @@
+import IntroductionTryTitle from "./IntroductionTryTitle";
+
 const IntroductionTry = () => {
   return (
     <div className={`flex flex-row justify-center bg-[#1AC573] p-[100px]`}>
-      <div className={`w-[1200px]`}></div>
+      <div className={`w-[1200px]`}>
+        <IntroductionTryTitle />
+      </div>
     </div>
   );
 };
---
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTryTitle.js
@@ -0,0 +1,14 @@
+const IntroductionTryTitle = () => {
+  return (
+    <div>
+      <p className={`text-white font-[Jua] text-[44px]`}>
+        AI 스피킹으로 우리 아이 영어 말하기
+      </p>
+      <p className={`text-white font-[Jua] text-[44px]`}>
+        지금 바로 시작하세요!
+      </p>
+    </div>
+  );
+};
+
+export default IntroductionTryTitle;
