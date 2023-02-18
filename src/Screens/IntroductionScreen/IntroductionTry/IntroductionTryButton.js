5a8311a8244ddcda41c1c4801622f3dc5402537d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 16:21:15 +0900

    [Add] 무료 체험 버튼 로직 추가.

--- src/Screens/IntroductionScreen/IntroductionTry/IntroductionTryButton.js
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTryButton.js
@@ -1,5 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 const IntroductionTryButton = () => {
-  const onClickTryButton = () => {};
+  const navigate = useNavigate();
+  const onClickTryButton = () => {
+    navigate("/demo");
+  };
   return (
     <p
       className={`inline-block text-white font-[Jua] text-[24px] px-[100px] py-[20px] border-2 border-white rounded-lg cursor-pointer`}
