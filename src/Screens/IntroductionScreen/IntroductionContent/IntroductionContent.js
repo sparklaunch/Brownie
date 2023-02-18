7a2df24bc2865f76dbf7d3a313b9a76147ffd998
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:18:47 +0900

    [Add] 인트로덕션 컨텐츠 윤곽선 추가.

--- src/Screens/IntroductionScreen/IntroductionContent/IntroductionContent.js
+++ src/Screens/IntroductionScreen/IntroductionContent/IntroductionContent.js
@@ -3,7 +3,9 @@ import IntroductionContentFeatures from "./IntroductionContentFeatures";
 
 const IntroductionContent = () => {
   return (
-    <div className={`flex flex-row justify-center bg-[#F9F9F9] p-[80px]`}>
+    <div
+      className={`flex flex-row justify-center bg-[#F9F9F9] p-[80px] border-t-[1px] border-b-[1px] border-[#DFDFDF]`}
+    >
       <div className={`w-[1200px] flex flex-col items-center`}>
         <IntroductionContentTitle />
         <IntroductionContentFeatures />
