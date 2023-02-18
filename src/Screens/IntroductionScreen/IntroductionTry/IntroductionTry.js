38eafe4da6f52ae3556ba81e5bb6c71e0ff419b2
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 16:08:14 +0900

    [Add] 무료 체험 섹션 멀티 디바이스 이미지 추가.

--- src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTry.js
@@ -1,12 +1,16 @@
 import IntroductionTryTitle from "./IntroductionTryTitle";
 import IntroductionTryButton from "./IntroductionTryButton";
+import IntroductionTryDevices from "./IntroductionTryDevices";
 
 const IntroductionTry = () => {
   return (
     <div className={`flex flex-row justify-center bg-[#1AC573] p-[100px]`}>
-      <div className={`w-[1200px]`}>
-        <IntroductionTryTitle />
-        <IntroductionTryButton />
+      <div className={`w-[1200px] flex flex-row justify-between`}>
+        <div>
+          <IntroductionTryTitle />
+          <IntroductionTryButton />
+        </div>
+        <IntroductionTryDevices />
       </div>
     </div>
   );
---
+++ src/Screens/IntroductionScreen/IntroductionTry/IntroductionTryDevices.js
@@ -0,0 +1,9 @@
+const IntroductionTryDevices = () => {
+  return (
+    <div>
+      <img src={`/assets/images/previews.png`} alt={`Devices`} />
+    </div>
+  );
+};
+
+export default IntroductionTryDevices;
