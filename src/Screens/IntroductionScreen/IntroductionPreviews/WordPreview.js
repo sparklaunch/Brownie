144e4a8e3e26aedce49044838020a5227fcf7d68
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:27:08 +0900

    [Add] 워드 모드 미리보기 추가.

--- src/Screens/IntroductionScreen/IntroductionPreviews/WordPreview.js
+++ src/Screens/IntroductionScreen/IntroductionPreviews/WordPreview.js
@@ -1,5 +1,12 @@
 const WordPreview = () => {
-  return <div></div>;
+  return (
+    <div className={`flex flex-col items-center`}>
+      <p className={`font-[Jua] text-[32px] mb-[16px]`}>Word 코너</p>
+      <p className={`text-[#515151] text-[18px]`}>발음을 듣고 따라 말하며</p>
+      <p className={`text-[#515151] text-[18px] mb-[60px]`}>단어를 익혀요.</p>
+      <img src={`/assets/images/word_preview.png`} alt={`Word Preview`} />
+    </div>
+  );
 };
 
 export default WordPreview;
