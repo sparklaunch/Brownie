5ded63f2695bd7328092e889b2a652416d77b6cf
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:10:30 +0900

    [Add] 인트로덕션 컨텐츠 배경색 지정.

--- src/Screens/IntroductionScreen/IntroductionContent/IntroductionContent.js
+++ src/Screens/IntroductionScreen/IntroductionContent/IntroductionContent.js
@@ -2,8 +2,8 @@ import IntroductionContentTitle from "./IntroductionContentTitle";
 
 const IntroductionContent = () => {
   return (
-    <div>
-      <div>
+    <div className={`flex flex-row justify-center bg-[#F9F9F9] p-[80px]`}>
+      <div className={`w-[1200px] flex flex-row justify-center`}>
         <IntroductionContentTitle />
       </div>
     </div>
