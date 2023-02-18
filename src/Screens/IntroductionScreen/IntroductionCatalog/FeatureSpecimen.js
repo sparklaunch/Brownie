41298bcdeb5019f07ca18d69991f2cd57dc40d06
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-18 15:56:20 +0900

    [Add] 피처 견본 완성.

--- src/Screens/IntroductionScreen/IntroductionCatalog/FeatureSpecimen.js
+++ src/Screens/IntroductionScreen/IntroductionCatalog/FeatureSpecimen.js
@@ -6,6 +6,12 @@ const FeatureSpecimen = () => {
         alt={`Feature`}
         className={`mb-[36px] w-[600px]`}
       />
+      <div className={`text-center`}>
+        <p className={`text-[#515151] text-[20px]`}>
+          파닉스 알파벳에 유의하여 읽을 수 있도록
+        </p>
+        <p className={`text-[#515151] text-[20px]`}>알파벳에 강조 색상 적용.</p>
+      </div>
     </div>
   );
 };
--- src/Screens/IntroductionScreen/IntroductionCatalog/IntroductionCatalogTitle.js
+++ src/Screens/IntroductionScreen/IntroductionCatalog/IntroductionCatalogTitle.js
@@ -1,6 +1,6 @@
 const IntroductionCatalogTitle = () => {
   return (
-    <div className={`flex flex-col items-center`}>
+    <div className={`flex flex-col items-center mb-[60px]`}>
       <p className={`font-[Jua] text-[44px]`}>
         파닉스 전용 교재 읽기 연습을 통한
       </p>
