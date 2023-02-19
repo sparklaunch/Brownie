0b56fb63503b03c056c00a52ea6cf6da3f63aa6a
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-19 13:26:25 +0900

    [Add] 테일윈드에서 스타일 컴포넌트로 전환.

--- src/Screens/ClassroomScreen/Word/Card.js
+++ src/Screens/ClassroomScreen/Word/Card.js
@@ -1,27 +1,13 @@
-import Instructions from "./Instructions";
-import WordCard from "./WordCard";
-import Pagination from "./Pagination";
-import Microphone from "./Microphone/Microphone";
-import ModeButtons from "../Mode/ModeButtons";
-import WordLeftPageTurner from "./Pager/WordLeftPageTurner";
-import WordRightPageTurner from "./Pager/WordRightPageTurner";
-import CardTitle from "./CardTitle";
-import HomeButton from "../HomeButton";
 import wordResultsShownAtom from "../../../Stores/Classroom/Word/wordResultsShown";
 import { useRecoilState } from "recoil";
-import WordResults from "./Results/WordResults";
 import wordMicrophoneStateAtom from "../../../Stores/Classroom/Word/wordMicrophoneState";
-import WordImage from "./WordImage";
-import WordWave from "./WordWave";
 import resultsScreenShownAtom from "../../../Stores/Classroom/Story/resultsScreenShown";
-import ResultsScreen from "../Story/Results/ResultsScreen";
 import wordScoresAtom from "../../../Stores/Classroom/Word/wordScores";
 import { useParams } from "react-router-dom";
 import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
 import youDidItShownAtom from "../../../Stores/Classroom/youDidItShown";
-import YouDidItScreen from "../Story/Results/YouDidIt/YouDidItScreen";
 import InsetBorder from "./InsetBorder";
-import styled from "styled-components";
+import { CardOuterContainer, InsetBorderContainer } from "./CardStyles";
 
 const Card = () => {
   const { level } = useParams();
@@ -38,106 +24,65 @@ const Card = () => {
   );
   const [youDidItShown, setYouDidItShown] = useRecoilState(youDidItShownAtom);
   return (
-    <CardContainer>
-      <div
-        className={`w-[75%] desktop:w-[94vh] h-full desktop:h-[62vh] relative bg-white desktop:rounded-2xl shadow-2xl`}
-      >
-        <div className={`absolute top-0 left-0 bottom-0 right-0`}>
-          <InsetBorder />
-        </div>
-        <div className={`absolute top-[20px] left-[140px]`}>
-          <Instructions />
-        </div>
-        <div className={`absolute top-0 left-0`}>
-          <Pagination />
-        </div>
-        <div
-          className={`absolute top-0 left-0 bottom-0 right-0 flex flex-col justify-center items-center`}
-        >
-          {wordMicrophoneState === "completed" ? <WordImage /> : <WordCard />}
-        </div>
-        <div className={`absolute top-[20px] desktop:top-0 left-[-120px]`}>
-          <ModeButtons />
-        </div>
-        <div
-          className={`absolute left-[-80px] bottom-[30px] desktop:top-[50%] desktop:left-[-80px] desktop:translate-y-[-50%]`}
-        >
-          <WordLeftPageTurner />
-        </div>
-        <div
-          className={`absolute right-[-80px] bottom-[30px] desktop:top-[50%] desktop:right-[-80px] desktop:translate-y-[-50%]`}
-        >
-          <WordRightPageTurner />
-        </div>
-        <div className={`absolute top-[-50px] left-0`}>
-          <CardTitle />
-        </div>
-        <div
-          className={`absolute top-[20px] right-[-110px] desktop:top-[-60px] desktop:right-[-100px]`}
-        >
-          <HomeButton />
-        </div>
-        {resultsScreenShown && (
-          <div>
-            <ResultsScreen />
-          </div>
-        )}
-        {wordScores[`${level}-${currentWordPage}`] !== undefined && (
-          <div className={`absolute top-0 left-[50%] translate-x-[-50%]`}>
-            <WordResults score={wordScores[`${level}-${currentWordPage}`]} />
-          </div>
-        )}
-        {wordMicrophoneState === "recording" && (
-          <div className={`absolute left-[50%] translate-x-[-50%] bottom-0`}>
-            <WordWave />
-          </div>
-        )}
-        {youDidItShown && (
-          <div className={`absolute top-0 left-0 bottom-0 right-0`}>
-            <YouDidItScreen />
-          </div>
-        )}
-        <Microphone />
-      </div>
-    </CardContainer>
+    <CardOuterContainer>
+      <InsetBorderContainer>
+        <InsetBorder />
+      </InsetBorderContainer>
+      {/*<div className={`absolute top-[20px] left-[140px]`}>*/}
+      {/*  <Instructions />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-0 left-0`}>*/}
+      {/*  <Pagination />*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className={`absolute top-0 left-0 bottom-0 right-0 flex flex-col justify-center items-center`}*/}
+      {/*>*/}
+      {/*  {wordMicrophoneState === "completed" ? <WordImage /> : <WordCard />}*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[20px] desktop:top-0 left-[-120px]`}>*/}
+      {/*  <ModeButtons />*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className={`absolute left-[-80px] bottom-[30px] desktop:top-[50%] desktop:left-[-80px] desktop:translate-y-[-50%]`}*/}
+      {/*>*/}
+      {/*  <WordLeftPageTurner />*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className={`absolute right-[-80px] bottom-[30px] desktop:top-[50%] desktop:right-[-80px] desktop:translate-y-[-50%]`}*/}
+      {/*>*/}
+      {/*  <WordRightPageTurner />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[-50px] left-0`}>*/}
+      {/*  <CardTitle />*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className={`absolute top-[20px] right-[-110px] desktop:top-[-60px] desktop:right-[-100px]`}*/}
+      {/*>*/}
+      {/*  <HomeButton />*/}
+      {/*</div>*/}
+      {/*{resultsScreenShown && (*/}
+      {/*  <div>*/}
+      {/*    <ResultsScreen />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*{wordScores[`${level}-${currentWordPage}`] !== undefined && (*/}
+      {/*  <div className={`absolute top-0 left-[50%] translate-x-[-50%]`}>*/}
+      {/*    <WordResults score={wordScores[`${level}-${currentWordPage}`]} />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*{wordMicrophoneState === "recording" && (*/}
+      {/*  <div className={`absolute left-[50%] translate-x-[-50%] bottom-0`}>*/}
+      {/*    <WordWave />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*{youDidItShown && (*/}
+      {/*  <div className={`absolute top-0 left-0 bottom-0 right-0`}>*/}
+      {/*    <YouDidItScreen />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*<Microphone />*/}
+    </CardOuterContainer>
   );
 };
-
-const CardContainer = styled.div`
-  @media (max-width: 767px) {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    overflow: clip;
-  }
-  @media (min-width: 768px) and (max-width: 1279px) {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    overflow: clip;
-  }
-  @media (min-width: 1280px) {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    overflow: clip;
-  }
-`;
 
 export default Card;
---
+++ src/Screens/ClassroomScreen/Word/CardStyles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const CardOuterContainer = styled.div`
+  @media (max-width: 767px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-left: 140px;
+    margin-right: 140px;
+  }
+  @media (min-width: 1280px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const InsetBorderContainer = styled.div`
+  @media (max-width: 767px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  @media (min-width: 1280px) {
+  }
+`;
+
+export { CardOuterContainer, InsetBorderContainer };
