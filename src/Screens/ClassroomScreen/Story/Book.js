e8c1626a0385f6d3579c1c2a941c5cf29ac59678
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-19 14:13:23 +0900

    [Add] 스토리 모드에도 스타일 컴포넌트 적용.

--- src/Screens/ClassroomScreen/Story/Book.js
+++ src/Screens/ClassroomScreen/Story/Book.js
@@ -1,17 +1,8 @@
-import BookContent from "./BookContent";
-import ModeButtons from "../Mode/ModeButtons";
-import LeftPageTurner from "./Pager/LeftPageTurner";
-import RightPageTurner from "./Pager/RightPageTurner";
-import PageControllers from "./Controllers/PageControllers";
-import HomeButton from "../HomeButton";
-import Navigator from "./Navigator/Navigator";
-import BookTitle from "./BookTitle";
-import CentralMicrophone from "./Microphone/Center/CentralMicrophone";
 import { useRecoilState } from "recoil";
 import centralMicrophoneStateAtom from "../../../Stores/Classroom/Story/Microphones/centralMicrophoneState";
-import Wave from "./Microphone/Center/Wave";
 import youDidItShownAtom from "../../../Stores/Classroom/youDidItShown";
-import YouDidItScreen from "./Results/YouDidIt/YouDidItScreen";
+import { BookContainer, BookContentContainer } from "./BookStyles";
+import BookContent from "./BookContent";
 
 const Book = () => {
   const [centralMicrophoneState, setCentralMicrophoneState] = useRecoilState(
@@ -19,56 +10,54 @@ const Book = () => {
   );
   const [youDidItShown, setYouDidItShown] = useRecoilState(youDidItShownAtom);
   return (
-    <div
-      className={`absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center overflow-clip`}
-    >
-      <div className={`w-[811px] h-[535px] relative`}>
-        <div className={`absolute top-0 left-[-120px]`}>
-          <ModeButtons />
-        </div>
-        <div className={`absolute top-[50%] left-[-80px] translate-y-[-50%]`}>
-          <LeftPageTurner />
-        </div>
-        <div className={`absolute top-[50%] right-[-80px] translate-y-[-50%]`}>
-          <RightPageTurner />
-        </div>
-        <div className={`absolute top-[-60px] right-0`}>
-          <PageControllers />
-        </div>
-        <div className={`absolute top-[-60px] right-[-100px]`}>
-          <HomeButton />
-        </div>
-        <div className={`absolute top-[-50px] left-0`}>
-          <BookTitle />
-        </div>
-        <div
-          className={`absolute bottom-[-100px] left-[50%] translate-x-[-50%]`}
-        >
-          <CentralMicrophone />
-        </div>
-        {centralMicrophoneState === "invisible" && (
-          <div
-            className={`absolute bottom-[10%] left-[50%] translate-x-[-50%] z-10`}
-          >
-            <Wave />
-          </div>
-        )}
-        {centralMicrophoneState === "loading" && (
-          <div
-            className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[3]`}
-          >
-            {/*<LoadingSpinner />*/}
-          </div>
-        )}
-        <Navigator />
+    <BookContainer>
+      {/*<div className={`absolute top-0 left-[-120px]`}>*/}
+      {/*  <ModeButtons />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[50%] left-[-80px] translate-y-[-50%]`}>*/}
+      {/*  <LeftPageTurner />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[50%] right-[-80px] translate-y-[-50%]`}>*/}
+      {/*  <RightPageTurner />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[-60px] right-0`}>*/}
+      {/*  <PageControllers />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[-60px] right-[-100px]`}>*/}
+      {/*  <HomeButton />*/}
+      {/*</div>*/}
+      {/*<div className={`absolute top-[-50px] left-0`}>*/}
+      {/*  <BookTitle />*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className={`absolute bottom-[-100px] left-[50%] translate-x-[-50%]`}*/}
+      {/*>*/}
+      {/*  <CentralMicrophone />*/}
+      {/*</div>*/}
+      {/*{centralMicrophoneState === "invisible" && (*/}
+      {/*  <div*/}
+      {/*    className={`absolute bottom-[10%] left-[50%] translate-x-[-50%] z-10`}*/}
+      {/*  >*/}
+      {/*    <Wave />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*{centralMicrophoneState === "loading" && (*/}
+      {/*  <div*/}
+      {/*    className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[3]`}*/}
+      {/*  >*/}
+      {/*    /!*<LoadingSpinner />*!/*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*<Navigator />*/}
+      <BookContentContainer>
         <BookContent />
-        {youDidItShown && (
-          <div className={`absolute top-0 left-0 bottom-0 right-0 z-[3]`}>
-            <YouDidItScreen />
-          </div>
-        )}
-      </div>
-    </div>
+      </BookContentContainer>
+      {/*{youDidItShown && (*/}
+      {/*  <div className={`absolute top-0 left-0 bottom-0 right-0 z-[3]`}>*/}
+      {/*    <YouDidItScreen />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+    </BookContainer>
   );
 };
 
---
+++ src/Screens/ClassroomScreen/Story/BookStyles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const BookContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const BookContentContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    margin-left: 140px;
+    margin-right: 140px;
+  }
+`;
+
+export { BookContainer, BookContentContainer };
