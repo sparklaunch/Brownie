3b955feabf18dfb7f1d9c3f17207b2f7ce92f1b0
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 00:58:30 +0900

    [Add] 내비게이션 바 그림자 추가.

--- src/Screens/MainScreen/NavigationBar/NavigationBarStyles.js
+++ src/Screens/MainScreen/NavigationBar/NavigationBarStyles.js
@@ -4,12 +4,8 @@ const NavigationBarOuterContainer = styled.div`
   @media (min-width: 767px) and (max-width: 1279px) {
     display: flex;
     justify-content: center;
-    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   }
   @media (min-width: 1280px) {
-    display: flex;
-    justify-content: center;
-    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   }
 `;
 
@@ -22,6 +18,7 @@ const NavigationBarInnerContainer = styled.div`
     justify-content: space-between;
     align-items: center;
     padding: 16px;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   }
   @media (min-width: 1280px) {
     display: flex;
