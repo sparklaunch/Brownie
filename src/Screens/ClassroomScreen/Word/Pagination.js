641c62dee8aa5d49997f05a66e3b3bc799a8cc8b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-19 11:54:53 +0900

    [Update] 워드 모드 페이지네이션 상태 업데이트.

--- src/Screens/ClassroomScreen/Word/Pagination.js
+++ src/Screens/ClassroomScreen/Word/Pagination.js
@@ -1,10 +1,20 @@
+import { useRecoilState } from "recoil";
+import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
+import useData from "../../../Hooks/useData";
+
 const Pagination = () => {
+  const words = useData("words");
+  const [currentWordPage, setCurrentWordPage] =
+    useRecoilState(currentWordPageAtom);
   return (
     <div className={`rounded-tl-2xl rounded-br-2xl bg-[#FFA63D]`}>
       <p
         className={`font-black text-[28px] tracking-widest px-[36px] py-[14px] text-white`}
       >
-        1<span className={`font-normal text-[20px] text-white`}>/9</span>
+        {currentWordPage}
+        <span className={`font-normal text-[20px] text-white`}>
+          /{words.length}
+        </span>
       </p>
     </div>
   );
