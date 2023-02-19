72bce80a0943d61a8d45bc13461e00d20320382d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 08:48:06 +0900

    [Add] 카탈로그 헤더 컨테이너 추가.

--- src/Screens/MainScreen/Catalog/CatalogHeader.js
+++ src/Screens/MainScreen/Catalog/CatalogHeader.js
@@ -1,10 +1,13 @@
+import {
+  CatalogHeaderContainer,
+  CatalogHeaderText
+} from "./CatalogHeaderStyles";
+
 const CatalogHeader = () => {
   return (
-    <div className={`py-[55px]`}>
-      <p className={`text-center font-[Jua] text-[44px]`}>
-        Phonics Fun Readers
-      </p>
-    </div>
+    <CatalogHeaderContainer>
+      <CatalogHeaderText>Phonics Fun Readers</CatalogHeaderText>
+    </CatalogHeaderContainer>
   );
 };
 
---
+++ src/Screens/MainScreen/Catalog/CatalogHeaderStyles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const CatalogHeaderContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding: 32px 0;
+  }
+  @media (min-width: 1280px) {
+    padding: 55px 0;
+  }
+`;
+
+const CatalogHeaderText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    text-align: center;
+    font-family: Jua, sans-serif;
+    font-size: 32px;
+  }
+  @media (min-width: 1280px) {
+    text-align: center;
+    font-family: Jua, sans-serif;
+    font-size: 44px;
+  }
+`;
+
+export { CatalogHeaderContainer, CatalogHeaderText };
