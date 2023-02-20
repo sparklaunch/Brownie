438da545126a545fba692c07f8093235c1c24096
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 10:06:51 +0900

    [Add] 푸터 컨테이너 추가.

--- src/Screens/MainScreen/Footer/Footer.js
+++ src/Screens/MainScreen/Footer/Footer.js
@@ -1,29 +1,38 @@
+import {
+  AddressContainer,
+  AddressText,
+  CopyrightContainer,
+  CopyrightText,
+  CustomerServiceNumberText,
+  DetailContainer,
+  FooterContainer,
+  NameText,
+  OwnerText,
+  RegistrationNumberText
+} from "./FooterStyles";
+
 const Footer = () => {
   return (
-    <div
-      className={`border-t-2 border-[#DFDFDF] p-[40px] flex flex-col items-center`}
-    >
-      <div
-        className={`flex flex-row items-center text-[14px] text-[#707070] py-1`}
-      >
-        <p className={`pr-3 border-r-[1px] border-[#DFDFDF]`}>(주)키즈브라운</p>
-        <p className={`pl-3`}>(04790) 서울특별시 성동구 성수일로 89, 605호</p>
-      </div>
-      <div
-        className={`flex flex-row items-center text-[14px] text-[#707070] py-1`}
-      >
-        <p className={`pr-3 border-r-[1px] border-[#DFDFDF]`}>대표 : 유필상</p>
-        <p className={`px-3 border-r-[1px] border-[#DFDFDF]`}>
+    <FooterContainer>
+      <AddressContainer>
+        <NameText>(주)키즈브라운</NameText>
+        <AddressText>(04790) 서울특별시 성동구 성수일로 89, 605호</AddressText>
+      </AddressContainer>
+      <DetailContainer>
+        <OwnerText>대표 : 유필상</OwnerText>
+        <RegistrationNumberText>
           사업자등록번호 : 539-81-00787
-        </p>
-        <p className={`pl-3`}>고객센터 : 02-512-8881</p>
-      </div>
-      <div className={`pt-3`}>
-        <p className={`text-[#ACACAC] text-[14px]`}>
+        </RegistrationNumberText>
+        <CustomerServiceNumberText>
+          고객센터 : 02-512-8881
+        </CustomerServiceNumberText>
+      </DetailContainer>
+      <CopyrightContainer>
+        <CopyrightText>
           © Kids Brown Partners Co. Lid.. All rights reserved.
-        </p>
-      </div>
-    </div>
+        </CopyrightText>
+      </CopyrightContainer>
+    </FooterContainer>
   );
 };
 
---
+++ src/Screens/MainScreen/Footer/FooterStyles.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    border-top: 2px solid #dfdfdf;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const AddressContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 14px;
+    color: #707070;
+    padding: 4px 0;
+  }
+`;
+
+const NameText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding-right: 8px;
+    border-right: 1px solid #dfdfdf;
+  }
+`;
+
+const AddressText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding-left: 8px;
+  }
+`;
+
+const DetailContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 14px;
+    color: #707070;
+    padding: 4px 0;
+  }
+`;
+
+const OwnerText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding-right: 8px;
+    border-right: 1px solid #dfdfdf;
+  }
+`;
+
+const RegistrationNumberText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding: 0 12px;
+    border-right: 1px solid #dfdfdf;
+  }
+`;
+
+const CustomerServiceNumberText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding-left: 12px;
+  }
+`;
+
+const CopyrightContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    padding-top: 10px;
+  }
+`;
+
+const CopyrightText = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 14px;
+    color: #acacac;
+  }
+`;
+
+export {
+  FooterContainer,
+  AddressContainer,
+  NameText,
+  AddressText,
+  DetailContainer,
+  OwnerText,
+  RegistrationNumberText,
+  CustomerServiceNumberText,
+  CopyrightContainer,
+  CopyrightText
+};
