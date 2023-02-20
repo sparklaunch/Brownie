be2ce1b072ec55af34a361372313adbdffbf20be
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 13:41:46 +0900

    [Add] 데스크탑 커션 적용.

--- src/Screens/MainScreen/Information/CautionStyles.js
+++ src/Screens/MainScreen/Information/CautionStyles.js
@@ -3,10 +3,20 @@ import styled from "styled-components";
 const CautionContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
   }
+  @media (min-width: 1280px) {
+  }
 `;
 
 const CautionTitle = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
+    background-color: #1ab9c5;
+    display: inline-block;
+    color: white;
+    font-size: 20px;
+    padding: 12px 20px;
+    border-radius: 8px;
+  }
+  @media (min-width: 1280px) {
     background-color: #1ab9c5;
     display: inline-block;
     color: white;
