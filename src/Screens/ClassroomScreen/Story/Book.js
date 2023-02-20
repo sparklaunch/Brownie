677705a6e29d955dd7cf7b0e15663197840ba7fa
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 17:57:04 +0900

    [Initialize] 교재 초기화... 썅...

--- src/Screens/ClassroomScreen/Story/Book.js
+++ src/Screens/ClassroomScreen/Story/Book.js
@@ -1,29 +1,8 @@
 import { useRecoilState } from "recoil";
 import centralMicrophoneStateAtom from "../../../Stores/Classroom/Story/Microphones/centralMicrophoneState";
 import youDidItShownAtom from "../../../Stores/Classroom/youDidItShown";
-import {
-  BookContainer,
-  BookContentContainer,
-  BookTitleContainer,
-  CentralMicrophoneContainer,
-  HomeButtonContainer,
-  LeftPageTurnerContainer,
-  ModeButtonsContainer,
-  NavigatorContainer,
-  PageControllersContainer,
-  RightPageTurnerContainer,
-  WaveContainer
-} from "./BookStyles";
-import BookContent from "./BookContent";
-import ModeButtons from "../Mode/ModeButtons";
-import LeftPageTurner from "./Pager/LeftPageTurner";
-import RightPageTurner from "./Pager/RightPageTurner";
-import PageControllers from "./Controllers/PageControllers";
-import HomeButton from "../HomeButton";
-import BookTitle from "./BookTitle";
-import CentralMicrophone from "./Microphone/Center/CentralMicrophone";
+import { BookContainer, NavigatorContainer } from "./BookStyles";
 import Navigator from "./Navigator/Navigator";
-import Wave from "./Microphone/Center/Wave";
 import resultsScreenShownAtom from "../../../Stores/Classroom/Story/resultsScreenShown";
 
 const Book = () => {
@@ -36,42 +15,6 @@ const Book = () => {
   const [youDidItShown, setYouDidItShown] = useRecoilState(youDidItShownAtom);
   return (
     <BookContainer>
-      <BookContentContainer>
-        <BookContent />
-      </BookContentContainer>
-      <ModeButtonsContainer>
-        <ModeButtons />
-      </ModeButtonsContainer>
-      <LeftPageTurnerContainer>
-        <LeftPageTurner />
-      </LeftPageTurnerContainer>
-      <RightPageTurnerContainer>
-        <RightPageTurner />
-      </RightPageTurnerContainer>
-      <PageControllersContainer>
-        <PageControllers />
-      </PageControllersContainer>
-      <HomeButtonContainer>
-        <HomeButton />
-      </HomeButtonContainer>
-      <BookTitleContainer>
-        <BookTitle />
-      </BookTitleContainer>
-      <CentralMicrophoneContainer>
-        <CentralMicrophone />
-      </CentralMicrophoneContainer>
-      {centralMicrophoneState === "invisible" && (
-        <WaveContainer>
-          <Wave />
-        </WaveContainer>
-      )}
-      {/*{centralMicrophoneState === "loading" && (*/}
-      {/*  <div*/}
-      {/*    className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[3]`}*/}
-      {/*  >*/}
-      {/*    /!*<LoadingSpinner />*!/*/}
-      {/*  </div>*/}
-      {/*)}*/}
       <NavigatorContainer>
         <Navigator />
       </NavigatorContainer>
