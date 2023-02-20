07a1d6a5994641195491dabc34df28a469db13ba
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 14:32:32 +0900

    [Add] 리절츠 스크린 데스크탑 적용.

--- src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreenStyles.js
+++ src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreenStyles.js
@@ -11,10 +11,31 @@ const ExcellentScreenContainer = styled.div`
     padding: 8px;
     z-index: 3;
   }
+  @media (min-width: 1280px) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 200px;
+    background-color: #80e6ac;
+    padding: 8px;
+    z-index: 3;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+  }
 `;
 
 const ExcellentImage = styled.img`
   @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+    height: 200px;
+    width: 300px;
+    scale: 0.8;
+  }
+  @media (min-width: 1280px) {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
--- src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreenStyles.js
+++ src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreenStyles.js
@@ -11,10 +11,31 @@ const GoodScreenContainer = styled.div`
     padding: 8px;
     z-index: 3;
   }
+  @media (min-width: 1280px) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 200px;
+    background-color: #ffc767;
+    padding: 8px;
+    z-index: 3;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+  }
 `;
 
 const GoodImage = styled.img`
   @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 10%;
+    height: 200px;
+    width: 185px;
+    scale: 0.8;
+  }
+  @media (min-width: 1280px) {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
