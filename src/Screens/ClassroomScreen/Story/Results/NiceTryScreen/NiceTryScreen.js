b13a0d2f9f1dabbe7069e87f3c242e3818f8a6a1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 10:34:12 +0900

    [Add] 나이스 트라이 컨테이너 추가.

--- src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreen.js
@@ -2,22 +2,20 @@ import { useRecoilState } from "recoil";
 import totalScoreAtom from "../../../../../Stores/Classroom/Story/totalScore";
 import ScorePill from "../Score/ScorePill";
 import { Slide } from "@mui/material";
+import { NiceTryImage, NiceTryScreenContainer } from "./NiceTryScreenStyles";
 
 const NiceTryScreen = () => {
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
   return (
     <Slide direction={`up`} in={true} mountOnEnter={true} unmountOnExit={true}>
-      <div
-        className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#FF92AC] p-8 z-[3] rounded-b-2xl`}
-      >
-        <img
+      <NiceTryScreenContainer>
+        <NiceTryImage
           src={`/assets/images/nicetry_frog.png`}
           alt={`Nice Try Frog`}
-          className={`absolute top-[50%] translate-y-[-50%] right-[5%] h-[200px] w-[215px] scale-[0.8]`}
         />
         <ScorePill />
         <p>{totalScore.score}</p>
-      </div>
+      </NiceTryScreenContainer>
     </Slide>
   );
 };
--- src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreenStyles.js
+++ src/Screens/ClassroomScreen/Story/Results/NiceTryScreen/NiceTryScreenStyles.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
-const GoodScreenContainer = styled.div`
+const NiceTryScreenContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
     position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
     height: 200px;
-    background-color: #ffc767;
+    background-color: #ff92ac;
     padding: 8px;
     z-index: 3;
     border-radius: 0 0 32px 32px;
   }
 `;
 
-const GoodImage = styled.img`
+const NiceTryImage = styled.img`
   @media (min-width: 768px) and (max-width: 1279px) {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
-    left: 10%;
+    right: 5%;
     height: 200px;
-    width: 185px;
+    width: 215px;
     scale: 0.8;
   }
 `;
 
-export { GoodScreenContainer, GoodImage };
+export { NiceTryScreenContainer, NiceTryImage };
