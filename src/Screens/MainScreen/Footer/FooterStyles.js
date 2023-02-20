dc0552f650a023583553b72f747072ac8ece2fcd
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 13:46:05 +0900

    [Add] 푸터 데스크탑 적용.

--- src/Screens/MainScreen/Footer/FooterStyles.js
+++ src/Screens/MainScreen/Footer/FooterStyles.js
@@ -8,10 +8,25 @@ const FooterContainer = styled.div`
     flex-direction: column;
     align-items: center;
   }
+  @media (min-width: 1280px) {
+    border-top: 2px solid #dfdfdf;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const AddressContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 14px;
+    color: #707070;
+    padding: 4px 0;
+  }
+  @media (min-width: 1280px) {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -26,16 +41,31 @@ const NameText = styled.p`
     padding-right: 8px;
     border-right: 1px solid #dfdfdf;
   }
+  @media (min-width: 1280px) {
+    padding-right: 8px;
+    border-right: 1px solid #dfdfdf;
+  }
 `;
 
 const AddressText = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
     padding-left: 8px;
   }
+  @media (min-width: 1280px) {
+    padding-left: 8px;
+  }
 `;
 
 const DetailContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 14px;
+    color: #707070;
+    padding: 4px 0;
+  }
+  @media (min-width: 1280px) {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -50,10 +80,18 @@ const OwnerText = styled.p`
     padding-right: 8px;
     border-right: 1px solid #dfdfdf;
   }
+  @media (min-width: 1280px) {
+    padding-right: 8px;
+    border-right: 1px solid #dfdfdf;
+  }
 `;
 
 const RegistrationNumberText = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
+    padding: 0 12px;
+    border-right: 1px solid #dfdfdf;
+  }
+  @media (min-width: 1280px) {
     padding: 0 12px;
     border-right: 1px solid #dfdfdf;
   }
@@ -63,16 +101,26 @@ const CustomerServiceNumberText = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
     padding-left: 12px;
   }
+  @media (min-width: 1280px) {
+    padding-left: 12px;
+  }
 `;
 
 const CopyrightContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
     padding-top: 10px;
   }
+  @media (min-width: 1280px) {
+    padding-top: 10px;
+  }
 `;
 
 const CopyrightText = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 14px;
+    color: #acacac;
+  }
+  @media (min-width: 1280px) {
     font-size: 14px;
     color: #acacac;
   }
