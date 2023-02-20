8bd55a68ea10963d9550a412f3ceeddf83b45e27
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 13:43:07 +0900

    [Add] 녹음 환경 데스크탑 적용.

--- src/Screens/MainScreen/Information/RecordingEnvironmentStyles.js
+++ src/Screens/MainScreen/Information/RecordingEnvironmentStyles.js
@@ -4,10 +4,19 @@ const RecordingEnvironmentContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
     margin: 12px 0 30px 0;
   }
+  @media (min-width: 1280px) {
+    margin: 12px 0 30px 0;
+  }
 `;
 
 const RecordingEnvironmentTitleContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 8px 0;
+  }
+  @media (min-width: 1280px) {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -21,6 +30,11 @@ const RecordingEnvironmentTitle = styled.p`
     font-size: 20px;
     margin-left: 10px;
   }
+  @media (min-width: 1280px) {
+    font-family: Jua, sans-serif;
+    font-size: 20px;
+    margin-left: 10px;
+  }
 `;
 
 const RecordingEnvironmentList = styled.ol`
@@ -28,10 +42,19 @@ const RecordingEnvironmentList = styled.ol`
     list-style: decimal;
     padding-left: 20px;
   }
+  @media (min-width: 1280px) {
+    list-style: decimal;
+    padding-left: 20px;
+  }
 `;
 
 const RecordingEnvironmentListItem = styled.li`
   @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 18px;
+    margin: 4px 0;
+    ${({ accented }) => accented && `color: #EC1C47;`}
+  }
+  @media (min-width: 1280px) {
     font-size: 18px;
     margin: 4px 0;
     ${({ accented }) => accented && `color: #EC1C47;`}
