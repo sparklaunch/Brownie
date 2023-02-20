d05994de915ff4f87a67fde278135fa5232a063c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 10:31:06 +0900

    [Add] 굳 스크린 컨테이너 추가.

--- src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreen.js
@@ -2,22 +2,17 @@ import { useRecoilState } from "recoil";
 import totalScoreAtom from "../../../../../Stores/Classroom/Story/totalScore";
 import ScorePill from "../Score/ScorePill";
 import { Slide } from "@mui/material";
+import { GoodImage, GoodScreenContainer } from "./GoodScreenStyles";
 
 const GoodScreen = () => {
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
   return (
     <Slide direction={`up`} in={true} mountOnEnter={true} unmountOnExit={true}>
-      <div
-        className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#FFC767] p-8 z-[3] rounded-b-2xl`}
-      >
-        <img
-          src={`/assets/images/good_monkey.png`}
-          alt={`Good Monkey`}
-          className={`absolute top-[50%] translate-y-[-50%] left-[10%] h-[200px] w-[185px] scale-[0.8]`}
-        />
+      <GoodScreenContainer>
+        <GoodImage src={`/assets/images/good_monkey.png`} alt={`Good Monkey`} />
         <ScorePill />
         <p>{totalScore.score}</p>
-      </div>
+      </GoodScreenContainer>
     </Slide>
   );
 };
---
+++ src/Screens/ClassroomScreen/Story/Results/GoodScreen/GoodScreenStyles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const GoodScreenContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 240px;
+    background-color: #ffc767;
+    padding: 8px;
+    z-index: 3;
+    border-radius: 0 0 32px 32px;
+  }
+`;
+
+const GoodImage = styled.img`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 10%;
+    height: 200px;
+    width: 185px;
+    scale: 0.8;
+  }
+`;
+
+export { GoodScreenContainer, GoodImage };
