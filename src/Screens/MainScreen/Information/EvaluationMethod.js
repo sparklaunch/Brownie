b39fe0eb615e83e266a2e84b6d23a23264928289
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 09:31:52 +0900

    [Add] 평가 방식 컨테이너 추가.

--- src/Screens/MainScreen/Information/EvaluationMethod.js
+++ src/Screens/MainScreen/Information/EvaluationMethod.js
@@ -1,16 +1,18 @@
+import {
+  EvaluationMethodContainer,
+  EvaluationMethodDescription,
+  EvaluationMethodTitle
+} from "./EvaluationMethodStyles";
+
 const EvaluationMethod = () => {
   return (
-    <div className={`flex flex-row items-center mb-[80px]`}>
-      <p
-        className={`bg-[#1AB9C5] inline-block text-white text-[20px] p-2 px-5 rounded-md`}
-      >
-        평가 방식
-      </p>
-      <p className={`ml-5 text-[18px]`}>
+    <EvaluationMethodContainer>
+      <EvaluationMethodTitle>평가 방식</EvaluationMethodTitle>
+      <EvaluationMethodDescription>
         <span className={`text-[#1AB9C5]`}>발음의 정확성과 유창성</span> 모두
         측정 가능한 AI 기술을 이용하여, 정밀하게 측정하여 자동 평가합니다.
-      </p>
-    </div>
+      </EvaluationMethodDescription>
+    </EvaluationMethodContainer>
   );
 };
 
---
+++ src/Screens/MainScreen/Information/EvaluationMethodStyles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const EvaluationMethodContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+`;
+
+const EvaluationMethodTitle = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 20px;
+    flex-shrink: 0;
+    padding: 12px 20px;
+    border-radius: 8px;
+    background-color: #1ab9c5;
+    color: white;
+    display: inline-block;
+  }
+`;
+
+const EvaluationMethodDescription = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 18px;
+    margin-left: 16px;
+  }
+`;
+
+export {
+  EvaluationMethodContainer,
+  EvaluationMethodTitle,
+  EvaluationMethodDescription
+};
