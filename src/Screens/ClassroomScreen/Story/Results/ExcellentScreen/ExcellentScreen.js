48ffed7d7ff6537ff8970858b6503acc86688ba5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 10:28:53 +0900

    [Add] 엑셀런트 스크린 컨테이너 추가.

--- src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreen.js
+++ src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreen.js
@@ -2,22 +2,23 @@ import { useRecoilState } from "recoil";
 import totalScoreAtom from "../../../../../Stores/Classroom/Story/totalScore";
 import ScorePill from "../Score/ScorePill";
 import { Slide } from "@mui/material";
+import {
+  ExcellentImage,
+  ExcellentScreenContainer
+} from "./ExcellentScreenStyles";
 
 const ExcellentScreen = () => {
   const [totalScore, setTotalScore] = useRecoilState(totalScoreAtom);
   return (
     <Slide direction={`up`} in={true} mountOnEnter={true} unmountOnExit={true}>
-      <div
-        className={`absolute bottom-0 left-0 right-0 h-[240px] bg-[#80E6AC] p-8 z-[3] rounded-b-2xl`}
-      >
-        <img
+      <ExcellentScreenContainer>
+        <ExcellentImage
           src={`/assets/images/excellent_bat.png`}
           alt={`Excellent bat`}
-          className={`absolute top-[50%] translate-y-[-50%] right-0 h-[200px] w-[300px] scale-[0.8]`}
         />
         <ScorePill />
         <p>{totalScore.score}</p>
-      </div>
+      </ExcellentScreenContainer>
     </Slide>
   );
 };
---
+++ src/Screens/ClassroomScreen/Story/Results/ExcellentScreen/ExcellentScreenStyles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const ExcellentScreenContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 240px;
+    background-color: #80e6ac;
+    padding: 8px;
+    z-index: 3;
+    border-radius: 0 0 32px 32px;
+  }
+`;
+
+const ExcellentImage = styled.img`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+    height: 200px;
+    width: 300px;
+    scale: 0.8;
+  }
+`;
+
+export { ExcellentScreenContainer, ExcellentImage };
