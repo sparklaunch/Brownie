fefaf656fd6862a8db001b169abafc0f7ecd07f0
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-20 09:42:16 +0900

    [Add] 녹음 환경 컨테이너 추가.

--- src/Screens/MainScreen/Information/RecordingEnvironment.js
+++ src/Screens/MainScreen/Information/RecordingEnvironment.js
@@ -1,23 +1,31 @@
+import {
+  RecordingEnvironmentContainer,
+  RecordingEnvironmentList,
+  RecordingEnvironmentListItem,
+  RecordingEnvironmentTitle,
+  RecordingEnvironmentTitleContainer
+} from "./RecordingEnvironmentStyles";
+
 const RecordingEnvironment = () => {
   return (
-    <div className={`my-3 mb-[40px]`}>
-      <div className={`flex flex-row items-center my-2`}>
+    <RecordingEnvironmentContainer>
+      <RecordingEnvironmentTitleContainer>
         <img
           src={"assets/images/icons/snowflake_icon.svg"}
           alt={"Snowflake Icon"}
         />
-        <p className={`font-[Jua] text-[20px] ml-[10px]`}>녹음 환경</p>
-      </div>
-      <ol className={`list-decimal pl-5`}>
-        <li className={`text-[18px] my-1`}>
+        <RecordingEnvironmentTitle>녹음 환경</RecordingEnvironmentTitle>
+      </RecordingEnvironmentTitleContainer>
+      <RecordingEnvironmentList>
+        <RecordingEnvironmentListItem>
           정확한 심사를 위해, 조용한 공간에서 녹음하고, 녹음 중 타인의 목소리가
           섞이지 않도록 주의해주세요.
-        </li>
-        <li className={`text-[18px] my-1 text-[#EC1C47]`}>
+        </RecordingEnvironmentListItem>
+        <RecordingEnvironmentListItem accented>
           정확한 테스트를 위해, 마이크를 이용해주세요.
-        </li>
-      </ol>
-    </div>
+        </RecordingEnvironmentListItem>
+      </RecordingEnvironmentList>
+    </RecordingEnvironmentContainer>
   );
 };
 
---
+++ src/Screens/MainScreen/Information/RecordingEnvironmentStyles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const RecordingEnvironmentContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    margin: 12px 0 30px 0;
+  }
+`;
+
+const RecordingEnvironmentTitleContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 8px 0;
+  }
+`;
+
+const RecordingEnvironmentTitle = styled.p`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    font-family: Jua, sans-serif;
+    font-size: 20px;
+    margin-left: 10px;
+  }
+`;
+
+const RecordingEnvironmentList = styled.ol`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    list-style: decimal;
+    padding-left: 20px;
+  }
+`;
+
+const RecordingEnvironmentListItem = styled.li`
+  @media (min-width: 768px) and (max-width: 1279px) {
+    font-size: 18px;
+    margin: 4px 0;
+    ${({ accented }) => accented && `color: #EC1C47;`}
+  }
+`;
+
+export {
+  RecordingEnvironmentContainer,
+  RecordingEnvironmentTitleContainer,
+  RecordingEnvironmentTitle,
+  RecordingEnvironmentList,
+  RecordingEnvironmentListItem
+};
