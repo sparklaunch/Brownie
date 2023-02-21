f44c85984601a321e048acca2719788e03f56153
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-22 07:53:13 +0900

    [Remove] 무료 체험판 리본 임시 제거.

--- src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
@@ -14,22 +14,22 @@ const Thumbnail = ({
     >
       <ThumbnailPage page={leftPage} pageDirection={"left"} />
       <ThumbnailPage page={rightPage} pageDirection={"right"} />
-      {completed && (
-        <div>
-          <div
-            className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}
-          />
-          <div
-            className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
-          >
-            <img
-              src={`/assets/images/thumbnail_completed_badge.svg`}
-              alt={`Completed Badge`}
-              className={`drop-shadow-2xl`}
-            />
-          </div>
-        </div>
-      )}
+      {/*{completed && (*/}
+      {/*  <div>*/}
+      {/*    <div*/}
+      {/*      className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}*/}
+      {/*    />*/}
+      {/*    <div*/}
+      {/*      className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}*/}
+      {/*    >*/}
+      {/*      <img*/}
+      {/*        src={`/assets/images/thumbnail_completed_badge.svg`}*/}
+      {/*        alt={`Completed Badge`}*/}
+      {/*        className={`drop-shadow-2xl`}*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*)}*/}
     </div>
   );
 };
