118edbea2cc6ac63f1bda0180834861b04234eb6
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-21 16:00:18 +0900

    [Update] 연락처 텍스트 비활성화.

--- src/Screens/MainScreen/NavigationBar/Contact.js
+++ src/Screens/MainScreen/NavigationBar/Contact.js
@@ -1,10 +1,10 @@
 const Contact = () => {
   return (
-    <div>
-      <a href={`tel:02-308-4088`} className={`flex flex-row items-center`}>
-        <img src={"assets/images/icons/phone_icon.svg"} alt={"Phone Icon"} />
-        <p className={`ml-[12px]`}>02-308-4088 (내선 2번)</p>
-      </a>
+    <div className={`flex flex-row items-center`}>
+      {/*<a href={`tel:02-308-4088`} className={`flex flex-row items-center`}>*/}
+      <img src={"assets/images/icons/phone_icon.svg"} alt={"Phone Icon"} />
+      <p className={`ml-[12px]`}>02-308-4088 (내선 2번)</p>
+      {/*</a>*/}
     </div>
   );
 };
