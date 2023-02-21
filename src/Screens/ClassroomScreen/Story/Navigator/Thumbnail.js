e18b5e248edda9557809f8f9892126f581cdc613
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-21 18:26:24 +0900

    [Add] 섬네일 완료 배지 디자인만 추가.

--- src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
@@ -1,14 +1,35 @@
 import ThumbnailPage from "./ThumbnailPage";
 
-const Thumbnail = ({ leftPage, rightPage, isCurrentPage = false }) => {
+const Thumbnail = ({
+  leftPage,
+  rightPage,
+  completed,
+  isCurrentPage = false
+}) => {
   return (
     <div
-      className={`w-[216px] h-[144px] border-4 grid grid-cols-2 rounded-lg cursor-pointer overflow-clip mx-3 ${
+      className={`relative w-[216px] h-[144px] border-4 grid grid-cols-2 rounded-lg cursor-pointer overflow-clip mx-3 ${
         isCurrentPage ? `border-[#FFD53D]` : `border-transparent`
       }`}
     >
       <ThumbnailPage page={leftPage} pageDirection={"left"} />
       <ThumbnailPage page={rightPage} pageDirection={"right"} />
+      {completed && (
+        <div>
+          <div
+            className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}
+          />
+          <div
+            className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+          >
+            <img
+              src={`/assets/images/thumbnail_completed_badge.svg`}
+              alt={`Completed Badge`}
+              className={`drop-shadow-2xl`}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Screens/ClassroomScreen/Story/Navigator/Thumbnails.js
+++ src/Screens/ClassroomScreen/Story/Navigator/Thumbnails.js
@@ -5,11 +5,15 @@ import { useRecoilState } from "recoil";
 import navigatorPageAtom from "../../../../Stores/Classroom/Story/navigatorPage";
 import currentPageAtom from "../../../../Stores/Classroom/Story/currentPage";
 import navigatorOpenAtom from "../../../../Stores/Classroom/Story/navigatorOpen";
+import scoresAtom from "../../../../Stores/Classroom/Story/scores";
+import { useParams } from "react-router-dom";
 
 const Thumbnails = () => {
+  const { level } = useParams();
   const [navigatorPage, setNavigatorPage] = useRecoilState(navigatorPageAtom);
   const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
   const [navigatorOpen, setNavigatorOpen] = useRecoilState(navigatorOpenAtom);
+  const [scores, setScores] = useRecoilState(scoresAtom);
   const onClickLeftEdgeThumbnail = () => {
     setNavigatorPage(Math.max(navigatorPage - 4, 0));
     setCurrentPage(Math.max(navigatorPage - 4, 0));
@@ -32,25 +36,48 @@ const Thumbnails = () => {
         <LeftEdgeThumbnail
           leftPage={navigatorPage - 4}
           rightPage={navigatorPage - 3}
+          completed={
+            scores[`${level}-${navigatorPage - 4}`] !== undefined &&
+            scores[`${level}-${navigatorPage - 3}`] !== undefined
+          }
         />
       </div>
       <div onClick={onClickLeftThumbnail}>
-        <Thumbnail leftPage={navigatorPage - 2} rightPage={navigatorPage - 1} />
+        <Thumbnail
+          leftPage={navigatorPage - 2}
+          rightPage={navigatorPage - 1}
+          completed={
+            scores[`${level}-${navigatorPage - 2}`] !== undefined &&
+            scores[`${level}-${navigatorPage - 1}`] !== undefined
+          }
+        />
       </div>
       <div>
         <Thumbnail
           leftPage={navigatorPage}
           rightPage={navigatorPage + 1}
           isCurrentPage={true}
+          completed={true}
         />
       </div>
       <div onClick={onClickRightThumbnail}>
-        <Thumbnail leftPage={navigatorPage + 2} rightPage={navigatorPage + 3} />
+        <Thumbnail
+          leftPage={navigatorPage + 2}
+          rightPage={navigatorPage + 3}
+          completed={
+            scores[`${level}-${navigatorPage + 2}`] !== undefined &&
+            scores[`${level}-${navigatorPage + 3}`] !== undefined
+          }
+        />
       </div>
       <div onClick={onClickRightEdgeThumbnail}>
         <RightEdgeThumbnail
           leftPage={navigatorPage + 4}
           rightPage={navigatorPage + 5}
+          completed={
+            scores[`${level}-${navigatorPage + 4}`] !== undefined &&
+            scores[`${level}-${navigatorPage + 5}`] !== undefined
+          }
         />
       </div>
     </div>
