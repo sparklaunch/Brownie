1a579c0136becbb7100d4960147ecf7c767feabf
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 04:36:09 +0900

    [Update] 점보트론 로그인 창에 드랍 섀도 필터 적용.

--- src/Screens/MainScreen/Jumbotron/JumbotronForm/SignInForm/SignInForm.js
+++ src/Screens/MainScreen/Jumbotron/JumbotronForm/SignInForm/SignInForm.js
@@ -5,14 +5,16 @@ import FormFooter from "./FormFooter";
 
 const SignInForm = () => {
   return (
-    <div
-      className={`w-[300px] h-[320px] rounded-lg shadow-2xl overflow-clip relative`}
-    >
-      <FormBackground />
-      <div className={`p-[16px] z-[1] absolute top-0 left-0 bottom-0 right-0`}>
-        <FormHeader />
-        <FormFields />
-        <FormFooter />
+    <div className={`drop-shadow-2xl`}>
+      <div className={`w-[300px] h-[320px] rounded-lg overflow-clip relative`}>
+        <FormBackground />
+        <div
+          className={`p-[16px] z-[1] absolute top-0 left-0 bottom-0 right-0`}
+        >
+          <FormHeader />
+          <FormFields />
+          <FormFooter />
+        </div>
       </div>
     </div>
   );
