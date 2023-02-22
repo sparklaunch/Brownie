8a957a1d75c492827706b8a8e43e3cb982ef4701
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 04:08:55 +0900

    [Update] 사용설명서 제거.

--- src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
+++ src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
@@ -4,9 +4,6 @@ import Contact from "./Contact";
 const NavigationContentTop = () => {
   return (
     <div className={`flex flex-row items-center justify-end mb-[24px]`}>
-      {/*<Link to={"/instructions"} className={`mr-[32px]`}>*/}
-      {/*  사용 설명서*/}
-      {/*</Link>*/}
       <Link to={"/administrator"} className={`mr-[32px]`}>
         관리자 사이트
       </Link>
