c85bf1cb3a4eff021c4a4a30568196147eb17c19
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 04:52:57 +0900

    [Update] 카탈로그 레벨 레이블 디자인 수정.

--- src/Screens/MainScreen/Catalog/CatalogLabelStyles.js
+++ src/Screens/MainScreen/Catalog/CatalogLabelStyles.js
@@ -3,32 +3,42 @@ import styled from "styled-components";
 const CatalogLabelContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
     position: absolute;
-    bottom: 55px;
-    left: 15px;
+    bottom: 40px;
+    left: 8px;
+    width: 67px;
+    height: 27px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: #1ab9c5;
+    border-radius: 5px;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
   @media (min-width: 1280px) {
     position: absolute;
-    bottom: 48px;
-    left: 12px;
+    bottom: 40px;
+    left: 8px;
+    width: 67px;
+    height: 27px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: #1ab9c5;
+    border-radius: 5px;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
 `;
 
 const CatalogLabelText = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
-    background-color: #1ab9c5;
     color: white;
     font-size: 14px;
-    padding: 4px 8px;
-    border-radius: 8px;
   }
   @media (min-width: 1280px) {
-    background-color: #1ab9c5;
     color: white;
     font-size: 14px;
-    padding: 4px 8px;
-    border-radius: 8px;
   }
 `;
 
