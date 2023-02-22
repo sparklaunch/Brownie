6f941477cf4a079a1fa9b1d8a9ca9467a54bb6b5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 03:48:36 +0900

    [Update] 회원가입, 로그인 창에서 로고 이미지 클릭 시 메인 화면으로 돌아가게 수정.

--- src/Screens/SignInScreen/SignUp/SignUpHeader.js
+++ src/Screens/SignInScreen/SignUp/SignUpHeader.js
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const SignUpHeader = () => {
+  const navigate = useNavigate();
   return (
     <div className={`flex flex-row justify-between items-center mb-3`}>
       <img
         src="/assets/images/logos/main_logo.png"
         alt="KidsBrown Logo"
-        className={`w-[180px]`}
+        className={`w-[180px] cursor-pointer`}
+        onClick={() => navigate("/")}
       />
       <p className={`text-[18px]`}>회원가입</p>
     </div>
