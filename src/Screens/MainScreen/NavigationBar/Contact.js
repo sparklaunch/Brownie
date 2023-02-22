21ba5c606d771fa19a6c22ccf8b623e1b91e5993
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 04:13:22 +0900

    [Update] 연락처의 수화기 아이콘 위치 변경.

--- src/Screens/MainScreen/NavigationBar/Contact.js
+++ src/Screens/MainScreen/NavigationBar/Contact.js
@@ -1,10 +1,12 @@
 const Contact = () => {
   return (
     <div className={`flex flex-row items-center`}>
-      <img src={"assets/images/icons/phone_icon.svg"} alt={"Phone Icon"} />
-      <p className={`ml-[12px] text-[#3D3D3D] text-[18px]`}>
-        02-308-4088 (내선 2번)
-      </p>
+      <img
+        src={"assets/images/icons/phone_icon.svg"}
+        alt={"Phone Icon"}
+        className={`translate-y-[-1px]`}
+      />
+      <p className={`ml-[12px] text-[#3D3D3D] text-[18px]`}>02-512-8881</p>
     </div>
   );
 };
