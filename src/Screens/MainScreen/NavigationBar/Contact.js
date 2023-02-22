f884c39dcabd62f11c919aaaaa244dffb867923a
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 04:10:55 +0900

    [Update] "관리자 사이트" 링크와 연락처 폰트 색상과 크기 변경.

--- src/Screens/MainScreen/NavigationBar/Contact.js
+++ src/Screens/MainScreen/NavigationBar/Contact.js
@@ -1,10 +1,10 @@
 const Contact = () => {
   return (
     <div className={`flex flex-row items-center`}>
-      {/*<a href={`tel:02-308-4088`} className={`flex flex-row items-center`}>*/}
       <img src={"assets/images/icons/phone_icon.svg"} alt={"Phone Icon"} />
-      <p className={`ml-[12px]`}>02-308-4088 (내선 2번)</p>
-      {/*</a>*/}
+      <p className={`ml-[12px] text-[#3D3D3D] text-[18px]`}>
+        02-308-4088 (내선 2번)
+      </p>
     </div>
   );
 };
--- src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
+++ src/Screens/MainScreen/NavigationBar/NavigationContentTop.js
@@ -4,7 +4,10 @@ import Contact from "./Contact";
 const NavigationContentTop = () => {
   return (
     <div className={`flex flex-row items-center justify-end mb-[24px]`}>
-      <Link to={"/administrator"} className={`mr-[32px]`}>
+      <Link
+        to={"/administrator"}
+        className={`mr-[24px] text-[#3D3D3D] text-[18px]`}
+      >
         관리자 사이트
       </Link>
       <Contact />
