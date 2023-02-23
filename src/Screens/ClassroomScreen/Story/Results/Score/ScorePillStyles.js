6215b931095dd392560bff203b175072eb2ee0f3
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-24 08:07:16 +0900

    [Update] 스코어필 디자인 수정.

--- src/Screens/ClassroomScreen/Story/Results/Score/ScorePillStyles.js
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScorePillStyles.js
@@ -6,12 +6,8 @@ const ScorePillOuterContainer = styled.div`
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    transform: scale(0.5);
   }
   @media (min-width: 993px) {
-    position: absolute;
-    top: 50%;
-    transform: translateX(-50%);
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -23,7 +19,7 @@ const ScorePillInnerContainer = styled.div`
   @media (min-width: 768px) and (max-width: 992px) {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     background-color: white;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
@@ -34,7 +30,7 @@ const ScorePillInnerContainer = styled.div`
   @media (min-width: 993px) {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     background-color: white;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
@@ -44,4 +40,4 @@ const ScorePillInnerContainer = styled.div`
   }
 `;
 
-export { ScorePillOuterContainer, ScorePillInnerContainer };
+export { ScorePillInnerContainer, ScorePillOuterContainer };
