a3ebf123176abeb4d37e6f4d5b1f22b340b40018
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 14:00:45 +0900

    [Update] 불필요한 파일 제거.

--- src/Screens/ClassroomScreen/Story/EmptyPage.js
+++
@@ -1,7 +0,0 @@
-import { EmptyPageContainer } from "./EmptyPageStyles";
-
-const EmptyPage = ({ direction }) => {
-  return <EmptyPageContainer direction={direction} />;
-};
-
-export default EmptyPage;
