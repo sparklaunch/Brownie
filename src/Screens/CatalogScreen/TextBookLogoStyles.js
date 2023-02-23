b0d8f653c75ebe0fa71c456fb2330739f96e66c1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 09:10:22 +0900

    [Add] 로고 하단 텍스트 간격 조정.

--- src/Screens/CatalogScreen/TextBookLogoStyles.js
+++ src/Screens/CatalogScreen/TextBookLogoStyles.js
@@ -34,6 +34,9 @@ const TextBookLogoDescription = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
   }
   @media (min-width: 1280px) {
+    &:first-child {
+      margin-right: 24px;
+    }
   }
 `;
 
