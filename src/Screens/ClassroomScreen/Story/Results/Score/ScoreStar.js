42cf9bc5f34ee1cfc64ea92b857a61055e970442
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 12:42:56 +0900

    [Update] 스코어 스타 스타일드 컴포넌트로 디자인 적용.

--- src/Screens/ClassroomScreen/Story/Results/Score/ScoreStar.js
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScoreStar.js
@@ -1,18 +1,18 @@
+import { ScoreStarImage } from "./ScoreStarStyles";
+
 const ScoreStar = ({ filled }) => {
   if (filled) {
     return (
-      <img
+      <ScoreStarImage
         src={`/assets/images/star_filled.svg`}
         alt={`Filled Star`}
-        className={`h-[26px] w-[26px] mx-1`}
       />
     );
   } else {
     return (
-      <img
+      <ScoreStarImage
         src={`/assets/images/star_empty.svg`}
         alt={`Empty Star`}
-        className={`h-[26px] w-[26px] mx-1`}
       />
     );
   }
---
+++ src/Screens/ClassroomScreen/Story/Results/Score/ScoreStarStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ScoreStarImage = styled.img`
+  @media (min-width: 768px) and (max-width: 992px) {
+    width: 26px;
+    height: 26px;
+  }
+  @media (min-width: 993px) {
+    width: 26px;
+    height: 26px;
+  }
+`;
+
+export { ScoreStarImage };
