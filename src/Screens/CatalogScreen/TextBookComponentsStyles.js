ade419ebfcaa3759a16c5a6b9ada87267526f9ae
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 09:49:05 +0900

    [Add] 교재 컴포넌츠 타이틀 추가.

--- src/Screens/CatalogScreen/TextBookComponentsStyles.js
+++ src/Screens/CatalogScreen/TextBookComponentsStyles.js
@@ -4,6 +4,9 @@ const TextBookComponentsContainer = styled.div`
   @media (min-width: 768px) and (max-width: 1279px) {
   }
   @media (min-width: 1280px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 `;
 
@@ -11,6 +14,14 @@ const TextBookComponentsTitle = styled.p`
   @media (min-width: 768px) and (max-width: 1279px) {
   }
   @media (min-width: 1280px) {
+    color: white;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 8px 20px;
+    background-color: #1ab9c5;
+    border-radius: 4px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    margin-bottom: 24px;
   }
 `;
 
