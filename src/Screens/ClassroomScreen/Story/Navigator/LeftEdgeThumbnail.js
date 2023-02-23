865916d1e00e04f70ba6d28e8dcc56b4bc4ac7c5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-23 11:53:48 +0900

    [Add] 내비게이터 섬네일 완료 배지 추가.

--- src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/LeftEdgeThumbnail.js
@@ -1,10 +1,15 @@
 import ThumbnailPage from "./ThumbnailPage";
 import EmptyThumbnail from "./EmptyThumbnail";
+import { useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import scoresAtom from "../../../../Stores/Classroom/Story/scores";
 
-const LeftEdgeThumbnail = ({ leftPage, rightPage, completed }) => {
-  if (completed) {
-    return <div>wow</div>;
-  }
+const LeftEdgeThumbnail = ({ leftPage, rightPage }) => {
+  const { level } = useParams();
+  const [scores, setScores] = useRecoilState(scoresAtom);
+  const completed = (function () {
+    return scores[`${level}-${leftPage}`] && scores[`${level}-${rightPage}`];
+  })();
   if (leftPage < 0) {
     return (
       <div
@@ -17,10 +22,26 @@ const LeftEdgeThumbnail = ({ leftPage, rightPage, completed }) => {
   }
   return (
     <div
-      className={`w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip mx-3 border-4 border-transparent cursor-pointer`}
+      className={`relative w-[216px] h-[144px] grid grid-cols-2 rounded-lg overflow-clip mx-3 border-4 border-transparent cursor-pointer`}
     >
       <ThumbnailPage isLeftEdge={true} page={leftPage} pageDirection={"left"} />
       <ThumbnailPage page={rightPage} pageDirection={"right"} />
+      {completed && (
+        <div>
+          <div
+            className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}
+          />
+          <div
+            className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+          >
+            <img
+              src={`/assets/images/thumbnail_completed_badge.svg`}
+              alt={`Completed Badge`}
+              className={`drop-shadow-2xl`}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
+++ src/Screens/ClassroomScreen/Story/Navigator/Thumbnail.js
@@ -1,11 +1,14 @@
 import ThumbnailPage from "./ThumbnailPage";
+import { useRecoilState } from "recoil";
+import scoresAtom from "../../../../Stores/Classroom/Story/scores";
+import { useParams } from "react-router-dom";
 
-const Thumbnail = ({
-  leftPage,
-  rightPage,
-  completed,
-  isCurrentPage = false
-}) => {
+const Thumbnail = ({ leftPage, rightPage, isCurrentPage = false }) => {
+  const { level } = useParams();
+  const [scores, setScores] = useRecoilState(scoresAtom);
+  const completed = (function () {
+    return scores[`${level}-${leftPage}`] && scores[`${level}-${rightPage}`];
+  })();
   return (
     <div
       className={`relative w-[216px] h-[144px] border-4 grid grid-cols-2 rounded-lg cursor-pointer overflow-clip mx-3 ${
@@ -14,22 +17,22 @@ const Thumbnail = ({
     >
       <ThumbnailPage page={leftPage} pageDirection={"left"} />
       <ThumbnailPage page={rightPage} pageDirection={"right"} />
-      {/*{completed && (*/}
-      {/*  <div>*/}
-      {/*    <div*/}
-      {/*      className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}*/}
-      {/*    />*/}
-      {/*    <div*/}
-      {/*      className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}*/}
-      {/*    >*/}
-      {/*      <img*/}
-      {/*        src={`/assets/images/thumbnail_completed_badge.svg`}*/}
-      {/*        alt={`Completed Badge`}*/}
-      {/*        className={`drop-shadow-2xl`}*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      {completed && (
+        <div>
+          <div
+            className={`absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50`}
+          />
+          <div
+            className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+          >
+            <img
+              src={`/assets/images/thumbnail_completed_badge.svg`}
+              alt={`Completed Badge`}
+              className={`drop-shadow-2xl`}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
