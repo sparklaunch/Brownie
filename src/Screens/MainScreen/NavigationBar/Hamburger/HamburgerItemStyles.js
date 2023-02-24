6d4b95003b6f75c7fa586927f7fe626b339a7d3d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-24 10:38:59 +0900

    [Add] 햄버거 아이템 디자인.

--- src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerItemStyles.js
+++ src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerItemStyles.js
@@ -2,19 +2,46 @@ import styled from "styled-components";
 
 const HamburgerItemContainer = styled.div`
   @media (min-width: 480px) and (max-width: 767px) {
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-top: 1px solid #dfdfdf;
+    &:last-of-type {
+      border-bottom: 1px solid #dfdfdf;
+    }
   }
   @media (min-width: 768px) and (max-width: 992px) {
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-top: 1px solid #dfdfdf;
   }
   @media (min-width: 993px) {
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-top: 1px solid #dfdfdf;
   }
 `;
 
 const HamburgerItemText = styled.p`
   @media (min-width: 480px) and (max-width: 767px) {
+    color: #222222;
+    font-weight: bold;
+    font-size: 14px;
   }
   @media (min-width: 768px) and (max-width: 992px) {
+    color: #222222;
+    font-weight: bold;
+    font-size: 14px;
   }
   @media (min-width: 993px) {
+    color: #222222;
+    font-weight: bold;
+    font-size: 14px;
   }
 `;
 
--- src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerItemsStyles.js
+++ src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerItemsStyles.js
@@ -4,14 +4,17 @@ const HamburgerItemsContainer = styled.div`
   @media (min-width: 480px) and (max-width: 767px) {
     display: flex;
     flex-direction: column;
+    margin: 20px 0;
   }
   @media (min-width: 768px) and (max-width: 992px) {
     display: flex;
     flex-direction: column;
+    margin: 20px 0;
   }
   @media (min-width: 993px) {
     display: flex;
     flex-direction: column;
+    margin: 20px 0;
   }
 `;
 
