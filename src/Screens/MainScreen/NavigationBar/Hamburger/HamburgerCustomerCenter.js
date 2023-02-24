3026c6d78ae886f6a7dbb745d7b944d6b5539a4e
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-24 10:50:38 +0900

    [Add] 고객센터 레이아웃 완성.

--- src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerCustomerCenter.js
+++ src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerCustomerCenter.js
@@ -1,12 +1,22 @@
 import {
+  ContactContainer,
+  ContactText,
   CustomerCenterText,
-  HamburgerCustomerCenterContainer
+  HamburgerCustomerCenterContainer,
+  PhoneIconImage
 } from "./HamburgerCustomerCenterStyles";
 
 const HamburgerCustomerCenter = () => {
   return (
     <HamburgerCustomerCenterContainer>
       <CustomerCenterText>고객센터</CustomerCenterText>
+      <ContactContainer>
+        <PhoneIconImage
+          src={`/assets/images/icons/phone_icon.svg`}
+          alt={`Phone Icon`}
+        />
+        <ContactText>02-512-8881</ContactText>
+      </ContactContainer>
     </HamburgerCustomerCenterContainer>
   );
 };
--- src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerCustomerCenterStyles.js
+++ src/Screens/MainScreen/NavigationBar/Hamburger/HamburgerCustomerCenterStyles.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 const HamburgerCustomerCenterContainer = styled.div`
   @media (min-width: 480px) and (max-width: 767px) {
+    margin-top: 40px;
   }
   @media (min-width: 768px) and (max-width: 992px) {
+    margin-top: 40px;
   }
   @media (min-width: 993px) {
+    margin-top: 40px;
   }
 `;
 
@@ -13,11 +16,75 @@ const CustomerCenterText = styled.p`
   @media (min-width: 480px) and (max-width: 767px) {
     font-size: 14px;
     color: #999;
+    margin-bottom: 10px;
   }
   @media (min-width: 768px) and (max-width: 992px) {
+    font-size: 14px;
+    color: #999;
+    margin-bottom: 10px;
   }
   @media (min-width: 993px) {
+    font-size: 14px;
+    color: #999;
+    margin-bottom: 10px;
   }
 `;
 
-export { HamburgerCustomerCenterContainer, CustomerCenterText };
+const ContactContainer = styled.div`
+  @media (min-width: 480px) and (max-width: 767px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  @media (min-width: 768px) and (max-width: 992px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  @media (min-width: 993px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`;
+
+const PhoneIconImage = styled.img`
+  @media (min-width: 480px) and (max-width: 767px) {
+    margin-right: 6px;
+    width: 14px;
+    height: 14px;
+  }
+  @media (min-width: 768px) and (max-width: 992px) {
+    margin-right: 6px;
+    width: 14px;
+    height: 14px;
+  }
+  @media (min-width: 993px) {
+    margin-right: 6px;
+    width: 14px;
+    height: 14px;
+  }
+`;
+
+const ContactText = styled.p`
+  @media (min-width: 480px) and (max-width: 767px) {
+    color: #3d3d3d;
+    font-size: 14px;
+  }
+  @media (min-width: 768px) and (max-width: 992px) {
+    color: #3d3d3d;
+    font-size: 14px;
+  }
+  @media (min-width: 993px) {
+    color: #3d3d3d;
+    font-size: 14px;
+  }
+`;
+
+export {
+  HamburgerCustomerCenterContainer,
+  CustomerCenterText,
+  ContactContainer,
+  PhoneIconImage,
+  ContactText
+};
