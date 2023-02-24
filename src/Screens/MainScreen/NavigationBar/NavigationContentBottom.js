088a71eed7ad147d076b126cafa49ca0c9ac8802
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-24 15:58:25 +0900

    [Update] 현재 URL에 따라 내비게이터 바텀 아이템 텍스트 색상 달라지게 수정.

--- src/Screens/MainScreen/NavigationBar/NavigationContentBottom.js
+++ src/Screens/MainScreen/NavigationBar/NavigationContentBottom.js
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SignInButton from "./SignButtons/SignInButton";
 import isLoggedIn from "../../../Utilities/isLoggedIn";
 import SignOutButton from "./SignButtons/SignOutButton";
 
 const NavigationContentBottom = () => {
+  const { pathname } = useLocation();
   return (
     <div className={`flex flex-row items-center justify-end`}>
       <Link
         to={"/"}
-        className={`mr-[24px] text-[18px] font-[700] text-[#1AB9C5]`}
+        className={`mr-[24px] text-[18px] font-[700] ${
+          pathname === "/" ? `text-[#1AB9C5]` : `text-[#222222]`
+        }`}
       >
         온라인 학습관
       </Link>
       <Link
         to={"/introduction"}
-        className={`mr-[24px] text-[18px] font-[700] text-[#222222]`}
+        className={`mr-[24px] text-[18px] font-[700] ${
+          pathname === "/introduction" ? `text-[#1AB9C5]` : `text-[#222222]`
+        }`}
       >
         프로그램 소개
       </Link>
       <Link
         to={"/catalog"}
-        className={`mr-[24px] text-[18px] font-[700] text-[#222222]`}
+        className={`mr-[24px] text-[18px] font-[700] ${
+          pathname === "/catalog" ? `text-[#1AB9C5]` : `text-[#222222]`
+        }`}
       >
         교재 구성
       </Link>
