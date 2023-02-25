9f4c74dc8fbd932d2135fd0a26283728d2bfe328
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-25 17:08:06 +0900

    [Update] 왼쪽 페이지 앵글 디자인 적용.

--- src/Screens/ClassroomScreen/Story/Controllers/LeftPageAngle.js
+++ src/Screens/ClassroomScreen/Story/Controllers/LeftPageAngle.js
@@ -1,15 +1,16 @@
+import {
+  LeftPageAngleContainer,
+  LeftPageAngleImage
+} from "./LeftPageAngleStyles";
+
 const LeftPageAngle = ({ disabled = false }) => {
   return (
-    <div
-      className={`w-[47px] h-[47px] bg-[#1AB9C5] rounded-full flex justify-center items-center shadow-md mr-2 ${
-        disabled ? `opacity-50` : `opacity-100 cursor-pointer`
-      }`}
-    >
-      <img
+    <LeftPageAngleContainer disabled={disabled}>
+      <LeftPageAngleImage
         src={"/assets/images/icons/left_angle.svg"}
         alt={"Left Angle Icon"}
       />
-    </div>
+    </LeftPageAngleContainer>
   );
 };
 
---
+++ src/Screens/ClassroomScreen/Story/Controllers/LeftPageAngleStyles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const LeftPageAngleContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 992px) {
+    width: 47px;
+    height: 47px;
+    background-color: #1ab9c5;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    margin-right: 8px;
+    ${(props) =>
+      props.disabled ? `opacity: 0.5;` : `opacity: 1; cursor: pointer;`}
+  }
+  @media (min-width: 993px) {
+    width: 47px;
+    height: 47px;
+    background-color: #1ab9c5;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    margin-right: 8px;
+    ${(props) =>
+      props.disabled ? `opacity: 0.5;` : `opacity: 1; cursor: pointer;`}
+  }
+`;
+
+const LeftPageAngleImage = styled.img`
+  @media (min-width: 768px) and (max-width: 992px) {
+  }
+  @media (min-width: 993px) {
+  }
+`;
+
+export { LeftPageAngleContainer, LeftPageAngleImage };
