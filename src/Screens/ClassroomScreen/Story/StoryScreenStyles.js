d2977bd60a2ededbdbf74fabcfe7259bc5674506
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-27 08:28:31 +0900

    [Update] 워드 스크린과 스토리 스크린에 오버플로우 클립 적용.

--- src/Screens/ClassroomScreen/Story/StoryScreenStyles.js
+++ src/Screens/ClassroomScreen/Story/StoryScreenStyles.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const StoryScreenContainer = styled.div`
   @media (min-width: 768px) and (max-width: 992px) {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    overflow: clip;
   }
   @media (min-width: 993px) {
     position: absolute;
