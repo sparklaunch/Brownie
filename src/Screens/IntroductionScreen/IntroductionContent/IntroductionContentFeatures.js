7b0c320035525eb817ed2f07c183626ef5add027
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-26 14:26:16 +0900

    [Update] 소개 페이지 컨텐츠 목록에 디자인 적용.

--- src/Screens/IntroductionScreen/IntroductionContent/IntroductionContentFeatures.js
+++ src/Screens/IntroductionScreen/IntroductionContent/IntroductionContentFeatures.js
@@ -1,8 +1,9 @@
 import IntroductionContentFeature from "./IntroductionContentFeature";
+import { IntroductionContentFeaturesContainer } from "./IntroductionContentFeaturesStyles";
 
 const IntroductionContentFeatures = () => {
   return (
-    <div className={`flex flex-col items-center`}>
+    <IntroductionContentFeaturesContainer>
       <IntroductionContentFeature
         icon={`finger_one`}
         text={`책 읽기를 통해 영어의 문자와 소리에 친숙해져요.`}
@@ -11,7 +12,7 @@ const IntroductionContentFeatures = () => {
         icon={`finger_two`}
         text={`읽기 연습을 통해 자신감을 키울 수 있어요.`}
       />
-    </div>
+    </IntroductionContentFeaturesContainer>
   );
 };
 
---
+++ src/Screens/IntroductionScreen/IntroductionContent/IntroductionContentFeaturesStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const IntroductionContentFeaturesContainer = styled.div`
+  @media (min-width: 768px) and (max-width: 992px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media (min-width: 993px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export { IntroductionContentFeaturesContainer };
