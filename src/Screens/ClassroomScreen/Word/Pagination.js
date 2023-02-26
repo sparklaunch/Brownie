cf1e091aab3502c47d1409a98c48870840316782
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-26 13:32:19 +0900

    [Update] 페이지네이션에 디자인 적용.

--- src/Screens/ClassroomScreen/Word/Pagination.js
+++ src/Screens/ClassroomScreen/Word/Pagination.js
@@ -1,20 +1,17 @@
 import { useRecoilState } from "recoil";
 import currentWordPageAtom from "../../../Stores/Classroom/Word/currentWordPage";
 import useData from "../../../Hooks/useData";
+import { PaginationText, PaginationTextSpan } from "./PaginationStyles";
 
 const Pagination = () => {
   const words = useData("words");
   const [currentWordPage, setCurrentWordPage] =
     useRecoilState(currentWordPageAtom);
   return (
-    <p
-      className={`font-black text-[28px] tracking-widest w-[120px] text-center py-[14px] text-white`}
-    >
+    <PaginationText>
       {currentWordPage}
-      <span className={`font-normal text-[20px] text-white`}>
-        /{words.length}
-      </span>
-    </p>
+      <PaginationTextSpan>/{words.length}</PaginationTextSpan>
+    </PaginationText>
   );
 };
 
---
+++ src/Screens/ClassroomScreen/Word/PaginationStyles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const PaginationText = styled.div`
+  @media (min-width: 768px) and (max-width: 992px) {
+    color: white;
+    font-size: 28px;
+    letter-spacing: 3px;
+    width: 120px;
+    text-align: center;
+    padding: 14px 0;
+    font-weight: bolder;
+  }
+  @media (min-width: 993px) {
+    color: white;
+    font-size: 28px;
+    letter-spacing: 3px;
+    width: 120px;
+    text-align: center;
+    padding: 14px 0;
+    font-weight: bolder;
+  }
+`;
+
+const PaginationTextSpan = styled.span`
+  @media (min-width: 768px) and (max-width: 992px) {
+    font-weight: normal;
+    font-size: 20px;
+    color: white;
+  }
+  @media (min-width: 993px) {
+    font-weight: normal;
+    font-size: 20px;
+    color: white;
+  }
+`;
+
+export { PaginationText, PaginationTextSpan };
