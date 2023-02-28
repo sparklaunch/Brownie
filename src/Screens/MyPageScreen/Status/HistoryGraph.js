9672767c44c4a89d8521662acfd48f1539b1ae17
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 19:01:20 +0900

    [Update] 최근 독서량 타이틀에 디자인 적용.

--- src/Screens/MyPageScreen/Status/HistoryGraph.js
+++ src/Screens/MyPageScreen/Status/HistoryGraph.js
@@ -1,9 +1,7 @@
+import { HistoryGraphTitle } from "./HistoryGraphStyles";
+
 const HistoryGraph = () => {
-  return (
-    <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
-      최근 월별 독서량
-    </p>
-  );
+  return <HistoryGraphTitle>최근 월별 독서량</HistoryGraphTitle>;
 };
 
 export default HistoryGraph;
---
+++ src/Screens/MyPageScreen/Status/HistoryGraphStyles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const HistoryGraphTitle = styled.p`
+  @media (480px <= width <= 767px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+  @media (768px <= width <= 992px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+  @media (width >= 993px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+`;
+
+export { HistoryGraphTitle };
