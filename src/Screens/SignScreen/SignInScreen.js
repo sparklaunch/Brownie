b07ca59940caa03cda34ab9dc1fc804fc315151c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 15:49:49 +0900

    [Update] 로그인, 회원가입 페이지 수정.

--- src/Screens/SignScreen/SignInScreen.js
+++ src/Screens/SignScreen/SignInScreen.js
@@ -4,6 +4,8 @@ import tabValueAtom from "../../Stores/Auth/tabValue";
 import SignInForm from "./SignIn/SignInForm";
 import SignUpForm from "./SignUp/SignUpForm";
 import Terms from "./Terms";
+import NavigationBar from "../MainScreen/NavigationBar/NavigationBar";
+import Footer from "../MainScreen/Footer/Footer";
 
 const SignInScreen = () => {
   const [tabValue, setTabValue] = useRecoilState(tabValueAtom);
@@ -11,22 +13,28 @@ const SignInScreen = () => {
     setTabValue(newValue);
   };
   return (
-    <div className={`flex flex-col mt-6 items-center h-full overflow-clip`}>
-      <div
-        className={`w-[500px] border-2 border-[#DFDFDF] rounded-lg shadow-lg`}
-      >
-        <Box sx={{ width: "100%" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs value={tabValue} onChange={onTabChange}>
-              <Tab label="로그인" className={`w-[50%]`} />
-              <Tab label="회원가입" className={`w-[50%]`} />
-            </Tabs>
-          </Box>
-          {tabValue === 0 ? <SignInForm /> : <SignUpForm />}
-        </Box>
+    <>
+      <NavigationBar />
+      <div className={`flex flex-col justify-center h-[100vh]`}>
+        <div className={`flex flex-col items-center overflow-clip`}>
+          <div
+            className={`w-[500px] border-2 border-[#DFDFDF] rounded-lg shadow-lg`}
+          >
+            <Box sx={{ width: "100%" }}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs value={tabValue} onChange={onTabChange}>
+                  <Tab label="로그인" className={`w-[50%]`} />
+                  <Tab label="회원가입" className={`w-[50%]`} />
+                </Tabs>
+              </Box>
+              {tabValue === 0 ? <SignInForm /> : <SignUpForm />}
+            </Box>
+          </div>
+          <Terms />
+        </div>
       </div>
-      <Terms />
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- src/Screens/SignScreen/SignUp/SignUpHeader.js
+++ src/Screens/SignScreen/SignUp/SignUpHeader.js
@@ -1,15 +1,6 @@
-import { useNavigate } from "react-router-dom";
-
 const SignUpHeader = () => {
-  const navigate = useNavigate();
   return (
-    <div className={`flex flex-row justify-between items-center mb-3`}>
-      <img
-        src="/assets/images/logos/main_logo.png"
-        alt="KidsBrown Logo"
-        className={`w-[180px] cursor-pointer`}
-        onClick={() => navigate("/")}
-      />
+    <div className={`flex flex-row justify-end items-center mb-3`}>
       <p className={`text-[18px]`}>회원가입</p>
     </div>
   );
