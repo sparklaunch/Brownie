3fc484c2ca4097131499bac8f95b8fe308dded0c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 19:05:34 +0900

    [Update] 내 정보에 디자인 적용.

--- src/Screens/MyPageScreen/Status/MyInformation.js
+++ src/Screens/MyPageScreen/Status/MyInformation.js
@@ -1,20 +1,22 @@
+import {
+  CouponCodeSpan,
+  MyInformationContainer,
+  MyInformationTitle,
+  PlainText
+} from "./MyInformationStyles";
+
 const MyInformation = () => {
   return (
     <>
-      <p className={`border-l-4 border-[#00AEA8] pl-3 mb-3 text-[20px]`}>
-        내 정보
-      </p>
-      <div
-        className={`p-8 border-[1px] border-[#DFDFDF] rounded-lg text-[18px] grid gap-3`}
-      >
-        <p>이름 : 홍길동</p>
-        <p>구매상품 : 패키지 A 상품</p>
-        <p>
-          이용쿠폰 :{" "}
-          <span className={`text-[#F34D7E]`}>A001 23 E01 06 0001</span>
-        </p>
-        <p>이용기간 : 2023.05.06 ~ 2023.12.31</p>
-      </div>
+      <MyInformationTitle>내 정보</MyInformationTitle>
+      <MyInformationContainer>
+        <PlainText>이름 : 홍길동</PlainText>
+        <PlainText>구매상품 : 패키지 A 상품</PlainText>
+        <PlainText>
+          이용쿠폰 : <CouponCodeSpan>A001 23 E01 06 0001</CouponCodeSpan>
+        </PlainText>
+        <PlainText>이용기간 : 2023.05.06 ~ 2023.12.31</PlainText>
+      </MyInformationContainer>
     </>
   );
 };
---
+++ src/Screens/MyPageScreen/Status/MyInformationStyles.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+const MyInformationTitle = styled.div`
+  @media (480px <= width <= 767px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+  @media (768px <= width <= 992px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+  @media (width >= 993px) {
+    border-left: 4px solid #00aea8;
+    padding-left: 12px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  }
+`;
+
+const MyInformationContainer = styled.div`
+  @media (480px <= width <= 767px) {
+    padding: 32px;
+    border: 1px solid #dfdfdf;
+    border-radius: 4px;
+    font-size: 18px;
+    display: grid;
+    gap: 12px;
+  }
+  @media (768px <= width <= 992px) {
+    padding: 32px;
+    border: 1px solid #dfdfdf;
+    border-radius: 4px;
+    font-size: 18px;
+    display: grid;
+    gap: 12px;
+  }
+  @media (width >= 993px) {
+    padding: 32px;
+    border: 1px solid #dfdfdf;
+    border-radius: 4px;
+    font-size: 18px;
+    display: grid;
+    gap: 12px;
+  }
+`;
+
+const PlainText = styled.p`
+  @media (480px <= width <= 767px) {
+  }
+  @media (768px <= width <= 992px) {
+  }
+  @media (width >= 993px) {
+  }
+`;
+
+const CouponCodeSpan = styled.span`
+  @media (480px <= width <= 767px) {
+    color: #f34d7e;
+  }
+  @media (768px <= width <= 992px) {
+    color: #f34d7e;
+  }
+  @media (width >= 993px) {
+    color: #f34d7e;
+  }
+`;
+
+export {
+  MyInformationTitle,
+  MyInformationContainer,
+  PlainText,
+  CouponCodeSpan
+};
