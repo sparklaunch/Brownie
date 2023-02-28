706ef2df3499351c2b5d51634d8c556c50468085
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:00:53 +0900

    [Update] 스토리 코너 프리뷰 디자인 적용.

--- src/Screens/IntroductionScreen/IntroductionPreviews/StoryPreview.js
+++ src/Screens/IntroductionScreen/IntroductionPreviews/StoryPreview.js
@@ -1,19 +1,21 @@
+import {
+  StoryCornerText,
+  StoryCornerTitle,
+  StoryPreviewContainer,
+  StoryPreviewImage
+} from "./StoryPreviewStyles";
+
 const StoryPreview = () => {
   return (
-    <div className={`flex flex-col items-center`}>
-      <p className={`font-[Jua] text-[32px] mb-[16px]`}>Story 코너</p>
-      <p className={`text-[#515151] text-[18px]`}>
-        원어민 발음을 듣고 따라해 보세요
-      </p>
-      <p className={`text-[#515151] text-[18px] mb-[60px]`}>
-        정확히 읽었는지 AI가 바로 알려줘요
-      </p>
-      <img
+    <StoryPreviewContainer>
+      <StoryCornerTitle>Story 코너</StoryCornerTitle>
+      <StoryCornerText>원어민 발음을 듣고 따라해 보세요</StoryCornerText>
+      <StoryCornerText>정확히 읽었는지 AI가 바로 알려줘요</StoryCornerText>
+      <StoryPreviewImage
         src={`/assets/images/story_preview.png`}
         alt={`Story Preview`}
-        className={`drop-shadow-2xl w-[384px]`}
       />
-    </div>
+    </StoryPreviewContainer>
   );
 };
 
