8063fb954de4868a431eae83bf1d239df5f4a2ae
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:41:56 +0900

    [Update] 카탈로그 타이틀에 디자인 적용.

--- src/Screens/MainScreen/Catalog/CatalogTitleStyles.js
+++ src/Screens/MainScreen/Catalog/CatalogTitleStyles.js
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
 const CatalogTitleContainer = styled.div`
+  @media (480px <= width <= 767px) {
+  }
   @media (768px <= width <= 992px) {
+  }
+  @media (width >= 993px) {
   }
 `;
 
 const CatalogTitleText = styled.p`
+  @media (480px <= width <= 767px) {
+    font-size: 16px;
+    background-color: #e8f6f8;
+    padding: 4px 16px;
+    margin: 4px 4px 0 4px;
+    border-radius: 8px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
   @media (768px <= width <= 992px) {
     font-size: 16px;
     background-color: #e8f6f8;
