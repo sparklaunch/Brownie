4b8f03b18c231c9a6cf011a42a2eea75ca21cf97
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:04:58 +0900

    [Update] 워드 코너 프리뷰 디자인 적용.

--- src/Screens/IntroductionScreen/IntroductionPreviews/WordPreview.js
+++ src/Screens/IntroductionScreen/IntroductionPreviews/WordPreview.js
@@ -1,15 +1,21 @@
+import {
+  WordCornerText,
+  WordCornerTitle,
+  WordPreviewContainer,
+  WordPreviewImage
+} from "./WordPreviewStyles";
+
 const WordPreview = () => {
   return (
-    <div className={`flex flex-col items-center`}>
-      <p className={`font-[Jua] text-[32px] mb-[16px]`}>Word 코너</p>
-      <p className={`text-[#515151] text-[18px]`}>발음을 듣고 따라 말하며</p>
-      <p className={`text-[#515151] text-[18px] mb-[60px]`}>단어를 익혀요</p>
-      <img
+    <WordPreviewContainer>
+      <WordCornerTitle>Word 코너</WordCornerTitle>
+      <WordCornerText>발음을 듣고 따라 말하며</WordCornerText>
+      <WordCornerText>단어를 익혀요</WordCornerText>
+      <WordPreviewImage
         src={`/assets/images/word_preview.png`}
         alt={`Word Preview`}
-        className={`drop-shadow-2xl`}
       />
-    </div>
+    </WordPreviewContainer>
   );
 };
 
---
+++ src/Screens/IntroductionScreen/IntroductionPreviews/WordPreviewStyles.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+const WordPreviewContainer = styled.div`
+  @media (480px <= width <= 767px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media (768px <= width <= 992px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media (width >= 993px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const WordCornerTitle = styled.p`
+  @media (480px <= width <= 767px) {
+    font-family: Jua, sans-serif;
+    font-size: 32px;
+    margin-bottom: 16px;
+  }
+  @media (768px <= width <= 992px) {
+    font-family: Jua, sans-serif;
+    font-size: 32px;
+    margin-bottom: 16px;
+  }
+  @media (width >= 993px) {
+    font-family: Jua, sans-serif;
+    font-size: 32px;
+    margin-bottom: 16px;
+  }
+`;
+
+const WordCornerText = styled.p`
+  @media (480px <= width <= 767px) {
+    color: #515151;
+    font-size: 18px;
+    &:last-of-type {
+      margin-bottom: 60px;
+    }
+  }
+  @media (768px <= width <= 992px) {
+    color: #515151;
+    font-size: 18px;
+    &:last-of-type {
+      margin-bottom: 60px;
+    }
+  }
+  @media (width >= 993px) {
+    color: #515151;
+    font-size: 18px;
+    &:last-of-type {
+      margin-bottom: 60px;
+    }
+  }
+`;
+
+const WordPreviewImage = styled.img`
+  @media (480px <= width <= 767px) {
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+  @media (768px <= width <= 992px) {
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+  @media (width >= 993px) {
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+`;
+
+export {
+  WordPreviewContainer,
+  WordCornerTitle,
+  WordCornerText,
+  WordPreviewImage
+};
