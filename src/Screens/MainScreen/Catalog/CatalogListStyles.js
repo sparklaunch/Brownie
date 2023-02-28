deec6f3abf57a325c0852d958025a8043e8ce885
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:40:17 +0900

    [Update] 카탈로그 리스트에 디자인 적용.

--- src/Screens/MainScreen/Catalog/CatalogListStyles.js
+++ src/Screens/MainScreen/Catalog/CatalogListStyles.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const CatalogListContainer = styled.div`
+  @media (480px <= width <= 767px) {
+  }
   @media (768px <= width <= 992px) {
+  }
+  @media (width >= 993px) {
   }
 `;
 
