4a70b2dce0de1bc0c1da5c2a2fa33e9da05a9eea
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:58:59 +0900

    [Update] 평가 방식에 디자인 적용.

--- src/Screens/MainScreen/Information/EvaluationMethod.js
+++ src/Screens/MainScreen/Information/EvaluationMethod.js
@@ -1,6 +1,7 @@
 import {
   EvaluationMethodContainer,
   EvaluationMethodDescription,
+  EvaluationMethodDescriptionSpan,
   EvaluationMethodTitle
 } from "./EvaluationMethodStyles";
 
@@ -9,8 +10,10 @@ const EvaluationMethod = () => {
     <EvaluationMethodContainer>
       <EvaluationMethodTitle>평가 방식</EvaluationMethodTitle>
       <EvaluationMethodDescription>
-        <span className={`text-[#1AB9C5]`}>발음의 정확성과 유창성</span> 모두
-        측정 가능한 AI 기술을 이용하여, 정밀하게 측정하여 자동 평가합니다
+        <EvaluationMethodDescriptionSpan>
+          발음의 정확성과 유창성
+        </EvaluationMethodDescriptionSpan>
+        모두 측정 가능한 AI 기술을 이용하여, 정밀하게 측정하여 자동 평가합니다
       </EvaluationMethodDescription>
     </EvaluationMethodContainer>
   );
--- src/Screens/MainScreen/Information/EvaluationMethodStyles.js
+++ src/Screens/MainScreen/Information/EvaluationMethodStyles.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 const EvaluationMethodContainer = styled.div`
+  @media (480px <= width <= 767px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 30px;
+  }
   @media (768px <= width <= 992px) {
     display: flex;
     flex-direction: row;
@@ -16,6 +22,15 @@ const EvaluationMethodContainer = styled.div`
 `;
 
 const EvaluationMethodTitle = styled.p`
+  @media (480px <= width <= 767px) {
+    font-size: 20px;
+    flex-shrink: 0;
+    padding: 12px 20px;
+    border-radius: 8px;
+    background-color: #1ab9c5;
+    color: white;
+    display: inline-block;
+  }
   @media (768px <= width <= 992px) {
     font-size: 20px;
     flex-shrink: 0;
@@ -37,6 +52,10 @@ const EvaluationMethodTitle = styled.p`
 `;
 
 const EvaluationMethodDescription = styled.p`
+  @media (480px <= width <= 767px) {
+    font-size: 18px;
+    margin-left: 16px;
+  }
   @media (768px <= width <= 992px) {
     font-size: 18px;
     margin-left: 16px;
@@ -47,8 +66,21 @@ const EvaluationMethodDescription = styled.p`
   }
 `;
 
+const EvaluationMethodDescriptionSpan = styled.span`
+  @media (480px <= width <= 767px) {
+    color: #1ab9c5;
+  }
+  @media (768px <= width <= 992px) {
+    color: #1ab9c5;
+  }
+  @media (width >= 993px) {
+    color: #1ab9c5;
+  }
+`;
+
 export {
   EvaluationMethodContainer,
   EvaluationMethodTitle,
-  EvaluationMethodDescription
+  EvaluationMethodDescription,
+  EvaluationMethodDescriptionSpan
 };
