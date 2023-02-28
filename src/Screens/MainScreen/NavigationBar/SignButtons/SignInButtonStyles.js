6778f37519fe91c2b247a870fb2de43eae7c0e84
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 17:54:06 +0900

    [Update] 로그인 버튼에 디자인 적용.

--- src/Screens/MainScreen/NavigationBar/SignButtons/SignInButtonStyles.js
+++ src/Screens/MainScreen/NavigationBar/SignButtons/SignInButtonStyles.js
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 
 const NavigationBarSignInButton = styled.button`
+  @media (480px <= width <= 767px) {
+    background-color: #1ab9c5;
+    filter: brightness(1) drop-shadow(0 0 5px rgba(0, 0, 0, 0.5));
+    font-family: Jua, sans-serif;
+    font-size: 18px;
+    width: 104px;
+    height: 44px;
+    border-radius: 8px;
+    color: white;
+  }
   @media (768px <= width <= 992px) {
+    background-color: #1ab9c5;
+    filter: brightness(1) drop-shadow(0 0 5px rgba(0, 0, 0, 0.5));
+    font-family: Jua, sans-serif;
+    font-size: 18px;
+    width: 104px;
+    height: 44px;
+    border-radius: 8px;
+    color: white;
   }
   @media (width >= 993px) {
     background-color: #1ab9c5;
