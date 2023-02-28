914dd6222f0228476842e01c422b7b1ff6c8e224
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-02-28 16:56:18 +0900

    [Update] 주의사항 스타일 적용.

--- src/Screens/MainScreen/Information/CautionStyles.js
+++ src/Screens/MainScreen/Information/CautionStyles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const CautionContainer = styled.div`
+  @media (480px <= width <= 767px) {
+  }
   @media (768px <= width <= 992px) {
   }
   @media (width >= 993px) {
@@ -8,6 +10,14 @@ const CautionContainer = styled.div`
 `;
 
 const CautionTitle = styled.p`
+  @media (480px <= width <= 767px) {
+    background-color: #1ab9c5;
+    display: inline-block;
+    color: white;
+    font-size: 20px;
+    padding: 12px 20px;
+    border-radius: 8px;
+  }
   @media (768px <= width <= 992px) {
     background-color: #1ab9c5;
     display: inline-block;
