00f33b5b471246e6ad47581a88ab76ec562d3ecb
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-02 14:57:23 +0900

    [Update] 쿠폰 사용 내역 그리드 수정.

--- src/Screens/MyPageScreen/CouponManagement/CouponHistory/CouponHistoryItemsStyles.js
+++ src/Screens/MyPageScreen/CouponManagement/CouponHistory/CouponHistoryItemsStyles.js
@@ -3,17 +3,26 @@ import styled from "styled-components";
 const CouponHistoryItemsContainer = styled.div`
   @media (480px <= width <= 767px) {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: 1fr;
     gap: 12px;
   }
   @media (768px <= width <= 992px) {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 1fr;
     gap: 12px;
   }
-  @media (width >= 993px) {
+  @media (993px <= width <= 1279px) {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 1fr;
+    gap: 12px;
+  }
+  @media (width >= 1280px) {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-auto-rows: 1fr;
     gap: 12px;
   }
 `;
