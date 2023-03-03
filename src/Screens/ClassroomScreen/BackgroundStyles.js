459d9fd3d2a3faf9d096016d0b7fb62d1d98cb1f
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-03 11:56:47 +0900

    [Update] 라이트 스카이블루 컬러 리팩토링.

--- src/Screens/ClassroomScreen/BackgroundStyles.js
+++ src/Screens/ClassroomScreen/BackgroundStyles.js
@@ -1,20 +1,21 @@
 import styled from "styled-components";
+import Constants from "../../Utilities/Constants";
 
 const BackgroundPlaceholder = styled.div`
   @media screen and (max-width: 767px) {
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     width: 100vw;
     height: 100vh;
   }
 
   @media screen and (min-width: 768px) and (max-width: 992px) {
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     width: 100vw;
     height: 100vh;
   }
 
   @media screen and (min-width: 993px) {
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     width: 100vw;
     height: 100vh;
   }
--- src/Screens/MainScreen/Catalog/CatalogTitleStyles.js
+++ src/Screens/MainScreen/Catalog/CatalogTitleStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Constants from "../../../Utilities/Constants";
 
 const CatalogTitleContainer = styled.div`
   @media screen and (max-width: 767px) {
@@ -14,21 +15,21 @@ const CatalogTitleContainer = styled.div`
 const CatalogTitleText = styled.p`
   @media screen and (max-width: 767px) {
     font-size: 16px;
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     padding: 4px 16px;
     margin: 4px 4px 0 4px;
     border-radius: 8px;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
     font-size: 16px;
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     padding: 4px 16px;
     margin: 4px 4px 0 4px;
     border-radius: 8px;
   }
   @media screen and (min-width: 993px) {
     font-size: 16px;
-    background-color: #e8f6f8;
+    background-color: ${Constants.LIGHT_SKYBLUE};
     padding: 4px 16px;
     margin: 4px 4px 0 4px;
     border-radius: 8px;
