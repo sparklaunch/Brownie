6047efa4ccef645d17c9766551f1d84dd44634e8
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-03 09:20:53 +0900

    [Update] "발음의 정확성과 유창성^모두 측정 가능한...".

--- src/Screens/MainScreen/Information/EvaluationMethod.js
+++ src/Screens/MainScreen/Information/EvaluationMethod.js
@@ -11,7 +11,7 @@ const EvaluationMethod = () => {
       <EvaluationMethodTitle>평가 방식</EvaluationMethodTitle>
       <EvaluationMethodDescription>
         <EvaluationMethodDescriptionSpan>
-          발음의 정확성과 유창성
+          발음의 정확성과 유창성{` `}
         </EvaluationMethodDescriptionSpan>
         모두 측정 가능한 AI 기술을 이용하여, 정밀하게 측정하여 자동 평가합니다
       </EvaluationMethodDescription>
