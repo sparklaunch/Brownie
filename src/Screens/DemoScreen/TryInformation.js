74627003cc8743e1fc9d6b51fa04ff126be16b68
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-03 16:07:43 +0900

    [Update] 체험하기 페이지 하단 정보 문구 추가.

--- src/Screens/DemoScreen/TryInformation.js
+++ src/Screens/DemoScreen/TryInformation.js
@@ -26,6 +26,15 @@ const TryInformation = () => {
           원하는 교재를 선택해주세요.
         </TryInformationItemText>
       </TryInformationItem>
+      <TryInformationItem>
+        <CheckImage
+          src={"assets/images/icons/check_icon.svg"}
+          alt={"Check Icon"}
+        />
+        <TryInformationItemText>
+          정확한 발음 평가를 위해 마이크를 사용해주세요.
+        </TryInformationItemText>
+      </TryInformationItem>
     </TryInformationContainer>
   );
 };
