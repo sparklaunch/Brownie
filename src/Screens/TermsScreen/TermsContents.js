4b02dd9f3a9631bf9b149832a1f5a21401ab6d19
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-06 14:21:03 +0900

    [Add] 개인정보취급방침 서두 추가.

--- src/Screens/TermsScreen/TermsContents.js
+++ src/Screens/TermsScreen/TermsContents.js
@@ -1,7 +1,42 @@
-import { TermsContentsContainer } from "./TermsContentsStyles";
+import {
+  TermsContentsContainer,
+  TermsItem,
+  TermsText,
+  TermsTitle
+} from "./TermsContentsStyles";
 
 const TermsContents = () => {
-  return <TermsContentsContainer></TermsContentsContainer>;
+  return (
+    <TermsContentsContainer>
+      <TermsText>
+        회원가입 서비스를 위하여 아래의 개인정보 수집, 이용에 대한 내용을 자세히
+        읽어보신 후, 동의 여부를 결정하여 주시기 바랍니다.
+      </TermsText>
+      <TermsItem>
+        <TermsTitle>1. 개인정보의 수집 및 이용 목적</TermsTitle>
+        <TermsText>
+          개인 식별, 부정 이용 및 비인가 사용 방지, 가입 의사 확인 등 회원가입
+          서비스를 제공하기 위함.
+        </TermsText>
+      </TermsItem>
+      <TermsItem>
+        <TermsTitle>2. 개인정보 수집 항목</TermsTitle>
+        <TermsText>- (필수) 아이디, 비밀번호, 휴대폰 번호, 학생 이름</TermsText>
+        <TermsText>- (선택) 학생의 생년월일</TermsText>
+      </TermsItem>
+      <TermsItem>
+        <TermsTitle>3. 개인정보 보유 및 이용 기간</TermsTitle>
+        <TermsText>
+          회원 탈퇴 시, 또는 타 법령에 근거하여 보유하며, 보유 기간 만료 시,
+          복구 불가능한 방법으로 삭제합니다.
+        </TermsText>
+        <TermsText>
+          위와 같은 개인정보 수집, 이용의 동의에 거부할 권리가 있습니다. 그러나
+          동의를 거부할 경우 회원가입 서비스를 제공받으실 수 없습니다.
+        </TermsText>
+      </TermsItem>
+    </TermsContentsContainer>
+  );
 };
 
 export default TermsContents;
--- src/Screens/TermsScreen/TermsContentsStyles.js
+++ src/Screens/TermsScreen/TermsContentsStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Constants from "../../Utilities/Constants";
 
 const TermsContentsContainer = styled.div`
   @media screen and (max-width: 767px) {
@@ -6,7 +7,45 @@ const TermsContentsContainer = styled.div`
   @media screen and (min-width: 768px) and (max-width: 992px) {
   }
   @media screen and (min-width: 993px) {
+    width: ${Constants.DESKTOP_CONTAINER_WIDTH};
+    background-color: ${Constants.LIGHT_GRAY};
+    border-radius: 8px;
+    padding: 32px;
+    margin: auto;
   }
 `;
 
-export { TermsContentsContainer };
+const TermsItem = styled.div`
+  @media screen and (max-width: 767px) {
+  }
+  @media screen and (min-width: 768px) and (max-width: 992px) {
+  }
+  @media screen and (min-width: 993px) {
+    margin: 16px 0;
+  }
+`;
+
+const TermsText = styled.p`
+  @media screen and (max-width: 767px) {
+  }
+  @media screen and (min-width: 768px) and (max-width: 992px) {
+  }
+  @media screen and (min-width: 993px) {
+    font-size: 14px;
+    line-height: 1.5;
+    margin: 4px 0;
+  }
+`;
+
+const TermsTitle = styled.p`
+  @media screen and (max-width: 767px) {
+  }
+  @media screen and (min-width: 768px) and (max-width: 992px) {
+  }
+  @media screen and (min-width: 993px) {
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
+
+export { TermsItem, TermsContentsContainer, TermsText, TermsTitle };
