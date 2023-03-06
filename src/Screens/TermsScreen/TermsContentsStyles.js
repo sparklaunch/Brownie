ccbdb9d059eb7596ad6db73987b9e322e5c365b0
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-06 15:20:33 +0900

    [Add] 개인정보취급방침 반응형 디자인 적용.

--- src/Screens/TermsScreen/TermsContentsStyles.js
+++ src/Screens/TermsScreen/TermsContentsStyles.js
@@ -3,22 +3,45 @@ import Constants from "../../Utilities/Constants";
 
 const TermsContentsContainer = styled.div`
   @media screen and (max-width: 767px) {
+    width: ${Constants.MOBILE_CONTAINER_WIDTH};
+    background-color: ${Constants.LIGHT_GRAY};
+    border-radius: 8px;
+    padding: 12px;
+    margin: 32px auto;
+    text-align: justify;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
+    width: ${Constants.TABLET_CONTAINER_WIDTH};
+    background-color: ${Constants.LIGHT_GRAY};
+    border-radius: 8px;
+    padding: 16px;
+    margin: 32px auto;
+    text-align: justify;
   }
-  @media screen and (min-width: 993px) {
+  @media screen and (min-width: 993px) and (max-width: 1279px) {
+    width: ${Constants.TABLET_CONTAINER_WIDTH};
+    background-color: ${Constants.LIGHT_GRAY};
+    border-radius: 8px;
+    padding: 24px;
+    margin: 32px auto;
+    text-align: justify;
+  }
+  @media screen and (min-width: 1280px) {
     width: ${Constants.DESKTOP_CONTAINER_WIDTH};
     background-color: ${Constants.LIGHT_GRAY};
     border-radius: 8px;
     padding: 32px;
     margin: 32px auto;
+    text-align: justify;
   }
 `;
 
 const TermsItem = styled.div`
   @media screen and (max-width: 767px) {
+    margin: 16px 0;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
+    margin: 16px 0;
   }
   @media screen and (min-width: 993px) {
     margin: 16px 0;
@@ -27,18 +50,29 @@ const TermsItem = styled.div`
 
 const TermsSubtitle = styled.p`
   @media screen and (max-width: 767px) {
+    font-size: 12px;
+    font-weight: bold;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
+    font-size: 14px;
+    font-weight: bold;
   }
   @media screen and (min-width: 993px) {
+    font-size: 16px;
     font-weight: bold;
   }
 `;
 
 const TermsText = styled.p`
   @media screen and (max-width: 767px) {
+    font-size: 10px;
+    line-height: 1.5;
+    margin: 2px 0;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
+    font-size: 12px;
+    line-height: 1.5;
+    margin: 3px 0;
   }
   @media screen and (min-width: 993px) {
     font-size: 14px;
@@ -49,8 +83,12 @@ const TermsText = styled.p`
 
 const TermsTitle = styled.p`
   @media screen and (max-width: 767px) {
+    font-size: 12px;
+    font-weight: bold;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
+    font-size: 14px;
+    font-weight: bold;
   }
   @media screen and (min-width: 993px) {
     font-size: 16px;
