dbff68d697d1d6d5f9ad83769729f0e5cc5ebe56
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-06 14:01:16 +0900

    [Add] 개인정보취급방침과 이용약관 링크 생성.

--- src/Screens/SignScreen/Terms.js
+++ src/Screens/SignScreen/Terms.js
@@ -1,15 +1,13 @@
 import { PrivacyLink, TermsContainer, TermsLink } from "./TermsStyles";
+import { useNavigate } from "react-router-dom";
 
 const Terms = () => {
-  // const navigate = useNavigate();
-  // const onClickPrivacy = () => {
-  //   navigate("/privacy");
-  // };
-  // const onClickTerms = () => {
-  //   navigate("/terms");
-  // };
+  const navigate = useNavigate();
+  const onClickTerms = () => {
+    navigate("/terms");
+  };
   return (
-    <TermsContainer>
+    <TermsContainer onClick={onClickTerms}>
       <PrivacyLink>개인정보취급방침</PrivacyLink>
       <TermsLink>이용약관</TermsLink>
     </TermsContainer>
--- src/Screens/SignScreen/TermsStyles.js
+++ src/Screens/SignScreen/TermsStyles.js
@@ -29,26 +29,32 @@ const PrivacyLink = styled.p`
   @media screen and (max-width: 767px) {
     padding-right: 12px;
     border-right: 2px solid ${Constants.LIGHT_GRAY};
+    cursor: pointer;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
     padding-right: 12px;
     border-right: 2px solid ${Constants.LIGHT_GRAY};
+    cursor: pointer;
   }
   @media screen and (min-width: 993px) {
     padding-right: 12px;
     border-right: 2px solid ${Constants.LIGHT_GRAY};
+    cursor: pointer;
   }
 `;
 
 const TermsLink = styled.p`
   @media screen and (max-width: 767px) {
     padding-left: 12px;
+    cursor: pointer;
   }
   @media screen and (min-width: 768px) and (max-width: 992px) {
     padding-left: 12px;
+    cursor: pointer;
   }
   @media screen and (min-width: 993px) {
     padding-left: 12px;
+    cursor: pointer;
   }
 `;
 
