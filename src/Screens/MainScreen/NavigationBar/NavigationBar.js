3e59e1112d583ec30a0b5eb66e0a6556fe45dfe5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-03-07 09:00:55 +0900

    [Add] 마우스휠 이벤트 핸들러 추가.

--- src/Screens/MainScreen/NavigationBar/NavigationBar.js
+++ src/Screens/MainScreen/NavigationBar/NavigationBar.js
@@ -4,12 +4,9 @@ import {
   NavigationBarInnerContainer,
   NavigationBarOuterContainer
 } from "./NavigationBarStyles";
-import { useRecoilValue } from "recoil";
-import manualOpenAtom from "../../../Stores/Misc/manualOpen";
 import ManualScreen from "../../ManualScreen/ManualScreen";
 
 const NavigationBar = () => {
-  const manualOpen = useRecoilValue(manualOpenAtom);
   return (
     <>
       <NavigationBarOuterContainer>
@@ -18,7 +15,7 @@ const NavigationBar = () => {
           <NavigationContent />
         </NavigationBarInnerContainer>
       </NavigationBarOuterContainer>
-      {manualOpen && <ManualScreen />}
+      <ManualScreen />
     </>
   );
 };
--- src/Screens/ManualScreen/ManualScreen.js
+++ src/Screens/ManualScreen/ManualScreen.js
@@ -5,13 +5,20 @@ import Popup from "reactjs-popup";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "reactjs-popup/dist/index.css";
+import { useRecoilValue } from "recoil";
+import manualOpenAtom from "../../Stores/Misc/manualOpen";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const ManualScreen = () => {
+  const onWheelManualContainer = (event) => {
+    const wheelDirection = event.deltaY > 0 ? "down" : "up";
+    console.log(wheelDirection);
+  };
+  const manualOpen = useRecoilValue(manualOpenAtom);
   return (
-    <Popup position={"right center"} closeOnDocumentClick={false} open={true}>
-      <ManualContainer>
+    <Popup position={"right center"} open={manualOpen}>
+      <ManualContainer onWheel={onWheelManualContainer}>
         <Document file={manualPDF}>
           <Page pageNumber={1} />
         </Document>
